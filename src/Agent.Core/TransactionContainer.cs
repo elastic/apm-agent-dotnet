19b0416963872c1d5fb2f165b1fe3f840671f9fd
Greg Kalapos <gergo@kalapos.net>
2018-11-09 19:41:28 +0100

    Remove depricated comment #4

--- src/Agent.Core/TransactionContainer.cs
+++ src/Agent.Core/TransactionContainer.cs
@@ -8,12 +8,8 @@ namespace Elastic.Agent.Core
     /// <summary>
     /// Transaction container storing and managing transactions that are in progress (started, but not ended)
     /// </summary>
-    public class TransactionContainer //TODO: make it internal and friend other elastic.apm dlls
+    public static class TransactionContainer //TODO: make it internal and friend other elastic.apm dlls
     {
-        //Dummy storage, we need logic here to store multiple onces across threads, etc.
-        //Plan: AsyncLocal<T>
-        //public static List<Transaction> Transactions { get; set; }
-
         public static AsyncLocal<List<Transaction>> Transactions { get; set; } = new AsyncLocal<List<Transaction>>();
     }
 }
