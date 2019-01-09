674c77a5f6b070fb99d16f5f1dc49db85d3e8175
Greg Kalapos <gergo@kalapos.net>
2019-01-09 19:38:03 +0100

    Adapt docs

--- src/Elastic.Apm/Model/Payload/Span.cs
+++ src/Elastic.Apm/Model/Payload/Span.cs
@@ -10,12 +10,12 @@ namespace Elastic.Apm.Model.Payload
 	public class Span : ISpan
 	{
 		public const string ActionExec = "exec";
-
 		public const string ActionQuery = "query";
 
 		public const string SubtypeHttp = "http";
 		public const string SubtypeMssql = "mssql";
 		public const string SubtypeSqLite = "sqlite";
+
 		public const string TypeDb = "db";
 		public const string TypeExternal = "external";
 
