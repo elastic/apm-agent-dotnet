705436da333f7bd79f9205e9886b15f51aca2188
Greg Kalapos <gergo@kalapos.net>
2019-03-17 13:00:45 +0100

    Add BenchmarkDotNet project

---
+++ test/Elastic.Apm.PerfTests/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Elastic.Apm.Tests.Mocks;
+
+namespace Elastic.Apm.PerfTests
+{
+	[MemoryDiagnoser]
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var summary = BenchmarkRunner.Run<Program>();
+		}
+
+
+		[Benchmark]
+		public void H100KTransactions100KSpans()
+		{
+			var agent = new ApmAgent(new AgentComponents(payloadSender: new MockPayloadSender()));
+
+			for (var i = 0; i < 100; i++)
+			{
+				agent.Tracer.CaptureTransaction("t", "t", (transaction) =>
+				{
+					for (var j = 0; j < 10; j++)
+					{
+						transaction.CaptureSpan("span", "perfSpan", () => { });
+					}
+				});
+			}
+		}
+	}
+}
