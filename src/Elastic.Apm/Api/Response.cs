3444970f5c5a5bf6cde7c8fb7653b293527fcf0e
Selçuk Ermaya <selcukermaya@gmail.com>
2019-03-18 20:24:05 +0300

    Fixed indentation.

--- src/Elastic.Apm/Api/Response.cs
+++ src/Elastic.Apm/Api/Response.cs
@@ -3,20 +3,20 @@ using Newtonsoft.Json;
 
 namespace Elastic.Apm.Api
 {
-    /// <summary>
-    /// Encapsulates Response related information that can be attached to an <see cref="ITransaction" /> through <see cref="ITransaction.Context" />
-    /// See <see cref="Context.Response" />
-    /// </summary>
-    public class Response
-    {
-        public bool Finished { get; set; }
+	/// <summary>
+	/// Encapsulates Response related information that can be attached to an <see cref="ITransaction" /> through <see cref="ITransaction.Context" />
+	/// See <see cref="Context.Response" />
+	/// </summary>
+	public class Response
+	{
+		public bool Finished { get; set; }
 
-        /// <summary>
-        /// The HTTP status code of the response.
-        /// </summary>
-        [JsonProperty("status_code")]
-        public int StatusCode { get; set; }
-        [JsonProperty("headers")]
-        public Dictionary<string, object> Headers { get; set; }
-    }
+		/// <summary>
+		/// The HTTP status code of the response.
+		/// </summary>
+		[JsonProperty("status_code")]
+		public int StatusCode { get; set; }
+		[JsonProperty("headers")]
+		public Dictionary<string, object> Headers { get; set; }
+	}
 }
