3cce2fa99a724abb26eae2f54945045fd5aa4323
Greg Kalapos <gergo@kalapos.net>
2019-03-21 10:41:47 +0100

    Add DebugLogSimple100Transaction10Spans

--- test/Elastic.Apm.PerfTests/Program.cs
+++ test/Elastic.Apm.PerfTests/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using BenchmarkDotNet.Attributes;
+﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using Elastic.Apm.Logging;
 using Elastic.Apm.Tests.Mocks;
 
 namespace Elastic.Apm.PerfTests
@@ -8,19 +8,39 @@ namespace Elastic.Apm.PerfTests
 	[MemoryDiagnoser]
 	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main()
 		{
-			var summary = BenchmarkRunner.Run<Program>();
+			BenchmarkRunner.Run<Program>();
 		}
 
 		[Benchmark]
-		public void SimmpleTransactions10Spans()
+		public void SimpleTransactions10Spans()
 		{
 			var agent = new ApmAgent(new AgentComponents(payloadSender: new MockPayloadSender()));
 
 			for (var i = 0; i < 100; i++)
 			{
-				agent.Tracer.CaptureTransaction("t", "t", (transaction) =>
+				agent.Tracer.CaptureTransaction("transaction", "perfTransaction", (transaction) =>
+				{
+					for (var j = 0; j < 10; j++)
+					{
+						transaction.CaptureSpan("span", "perfSpan", () => { });
+					}
+				});
+			}
+		}
+
+		[Benchmark]
+		public void DebugLogSimple100Transaction10Spans()
+		{
+			var testLogger = new PerfTestLogger(LogLevel.Debug);
+			var agent = new ApmAgent(new AgentComponents(payloadSender: new MockPayloadSender(), logger: testLogger,
+				configurationReader: new TestAgentConfigurationReader(testLogger, logLevel: "Debug")));
+
+
+			for (var i = 0; i < 100; i++)
+			{
+				agent.Tracer.CaptureTransaction("transaction", "perfTransaction", (transaction) =>
 				{
 					for (var j = 0; j < 10; j++)
 					{
