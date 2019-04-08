11c686e2a96e810ae105e797b79ef46ade92169a
Greg Kalapos <gergo@kalapos.net>
2019-04-08 16:57:41 +0200

    Update sample/WebApiSample/Startup.cs

--- sample/WebApiSample/Startup.cs
+++ sample/WebApiSample/Startup.cs
@@ -16,8 +16,11 @@ namespace WebApiSample
 {
 	public class Startup
 	{
+		private readonly IConfiguration _configuration;
+
 		public Startup(IConfiguration configuration)
 		{
+			_configuration = configuration;
 		}
 
 		// This method gets called by the runtime. Use this method to add services to the container.
@@ -29,7 +32,7 @@ namespace WebApiSample
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
-			app.UseElasticApm();
+			app.UseElasticApm(_configuration);
 			ConfigureAllExceptAgent(app);
 		}
 
