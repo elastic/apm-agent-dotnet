2ed69c3a1ff6f435cc9588be7092501d82f560fd
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2019-04-19 09:30:30 +0300

    Minor fixes in Sampler.cs

--- src/Elastic.Apm/Sampler.cs
+++ src/Elastic.Apm/Sampler.cs
@@ -1,15 +1,13 @@
 using System;
-using Elastic.Apm.Helpers;
 
 namespace Elastic.Apm
 {
-
 	/// <summary>
-	/// A sampler is responsible for determining whether a <see cref="Transaction"/> should be sampled.
+	/// A sampler is responsible for determining whether a transaction should be sampled.
 	/// In contrast to other tracing systems, in Elastic APM,  non-sampled transactions do get reported to the APM server.
-	/// However, to keep the size at a minimum, the reported transaction only contains the transaction name, the duration and the id.
-	/// Also, <see cref="Span"/>s of non-sampled transactions are not reported.
-	///
+	/// However, to keep the size at a minimum, the reported transaction only contains the transaction name, the duration and
+	/// the id.
+	/// Also, spans of non-sampled transactions are not reported.
 	/// This implementation samples based on a sampling probability (AKA sampling rate) between 0.0 and 1.0.
 	/// A sampling rate of 0.5 means that 50% of all transactions should be sampled.
 	/// </summary>
@@ -17,7 +15,7 @@ namespace Elastic.Apm
 	{
 		private readonly bool _constantValue;
 		private readonly bool _isConstant;
-		private readonly UInt64 _maxSampledUInt64;
+		private readonly ulong _maxSampledUInt64;
 
 		/// <summary>
 		/// Constructs a new Sampler
@@ -39,20 +37,25 @@ namespace Elastic.Apm
 				case 1:
 					_isConstant = true;
 					_constantValue = true;
-					_maxSampledUInt64 = UInt64.MaxValue;
+					_maxSampledUInt64 = ulong.MaxValue;
 					break;
 				default:
-					_maxSampledUInt64 = Convert.ToUInt64(UInt64.MaxValue * rate);
+					_maxSampledUInt64 = Convert.ToUInt64(ulong.MaxValue * rate);
 					_isConstant = false;
 					break;
 			}
 		}
 
+		internal bool? Constant => _isConstant ? _constantValue : default(bool?);
+
 		/// <summary>
 		/// Decides if to sample or not based on the given randomBytes.
 		/// </summary>
 		/// <param name="randomBytes">Should contain at least 8 random bytes.</param>
-		/// <exception cref="System.ArgumentException">When length of <paramref name="randomBytes">randomBytes</paramref> is less than 8.</exception>
+		/// <exception cref="System.ArgumentException">
+		/// When length of <paramref name="randomBytes">randomBytes</paramref> is less
+		/// than 8.
+		/// </exception>
 		/// <returns>True if and only if the decision is to sample</returns>
 		internal bool DecideIfToSample(byte[] randomBytes)
 		{
@@ -62,7 +65,5 @@ namespace Elastic.Apm
 		}
 
 		internal static bool IsValidRate(double rate) => 0 <= rate && rate <= 1.0;
-
-		internal bool? Constant => _isConstant ? _constantValue : default(bool?);
 	}
 }
