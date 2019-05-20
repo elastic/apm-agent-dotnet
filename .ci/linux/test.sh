104d5b92ac34cef8be87058d3c787d557c54075b
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-05-20 20:44:47 +0100

    Ident

--- .ci/linux/test.sh
+++ .ci/linux/test.sh
@@ -9,10 +9,10 @@ set -euxo pipefail
 dotnet test -v n -r target -d target/diag.log --no-build \
     --logger:"xunit;LogFilePath=TestResults.xml" \
     /p:CollectCoverage=true \
-	/p:CoverletOutputFormat=cobertura \
+    /p:CoverletOutputFormat=cobertura \
     /p:CoverletOutput=target/Coverage/ \
     /p:Exclude='"[Elastic.Apm.Tests]*,[SampleAspNetCoreApp*]*,[xunit*]*"' \
     /p:Threshold=0 \
-	/p:ThresholdType=branch \
-	/p:ThresholdStat=total \
-	|| echo -e "\033[31;49mTests FAILED\033[0m"
+    /p:ThresholdType=branch \
+    /p:ThresholdStat=total \
+    || echo -e "\033[31;49mTests FAILED\033[0m"
