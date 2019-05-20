fe4e2fba7ebb2213bbf19f46c27b79d98d5a7b0e
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-05-20 19:49:37 +0100

    Enforce test log file

--- .ci/linux/convert.sh
+++ .ci/linux/convert.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-#convert xunit files to junit files
-for i in $(find . -name TestResults.xml)
+# convert xunit files to junit files
+for i in $(find . -name TestResult.xml)
 do
 	DIR=$(dirname "$i")
 	dotnet xunit-to-junit "$i" "${DIR}/junit-testTesults.xml"
--- .ci/windows/convert.ps1
+++ .ci/windows/convert.ps1
@@ -1,5 +1,5 @@
 [System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";" + $Env:USERPROFILE + "\\.dotnet\\tools")
-Get-ChildItem -Path . -Recurse -Filter TestResults.xml |
+Get-ChildItem -Path . -Recurse -Filter TestResult.xml |
 Foreach-Object {
     & dotnet xunit-to-junit $_.FullName $_.parent.FullName + '\\junit-testTesults.xml'
 }
