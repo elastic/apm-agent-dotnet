fd0cf3a904b669da4d45f9125e2e0e607828a31f
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-05-21 10:35:58 +0100

    Revert "For debugging purposes"
    This reverts commit 8dbb820deec2b6909aec18131c3ce1e2d8b28b31.

--- .ci/linux/release.sh
+++ .ci/linux/release.sh
@@ -5,11 +5,6 @@
 #
 set -euxo pipefail
 
-# For debugging purposes
-env | sort
-ls -l ${DOTNET_ROOT}
-find ${DOTNET_ROOT} -type f -ls
-
 # Remove sample projects
 dotnet sln remove test/Elastic.Apm.PerfTests/Elastic.Apm.PerfTests.csproj
 dotnet sln remove src/Elastic.Apm.AspNetFullFramework/Elastic.Apm.AspNetFullFramework.csproj
