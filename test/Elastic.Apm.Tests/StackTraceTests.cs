c4cfc9e79e14d4694c1d7bfdaa36858fec132e80
Greg Kalapos <gergo@kalapos.net>
2019-06-01 00:31:01 +0200

    Adding tests covering async callstack scenarios

--- test/Elastic.Apm.Tests/StackTraceTests.cs
+++ test/Elastic.Apm.Tests/StackTraceTests.cs
@@ -1,5 +1,7 @@
+using System;
+using System.Linq;
 using System.Net.Http;
-using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Elastic.Apm.Model;
 using Elastic.Apm.Tests.Mocks;
@@ -35,6 +37,72 @@ namespace Elastic.Apm.Tests
 			var stackFrames = payloadSender.FirstSpan?.StackTrace;
 
 			stackFrames.Should().NotBeEmpty().And.Contain(frame => frame.LineNo != 0);
+		}
+
+		/// <summary>
+		/// Makes sure that the name of the async method is captured correctly
+		/// </summary>
+		[Fact]
+		public async Task AsyncCallStackTest()
+		{
+			var payloadSender = new MockPayloadSender();
+			var agent = new ApmAgent(new TestAgentComponents(payloadSender: payloadSender));
+
+			await Assert.ThrowsAsync<Exception>(async () =>
+			{
+				await agent.Tracer.CaptureTransaction("TestTransaction", "Test", async () =>
+				{
+					var classWithAsync = new ClassWithAsync();
+					await classWithAsync.TestMethodAsync();
+				});
+			});
+
+			payloadSender.Errors.Should().NotBeEmpty();
+			(payloadSender.Errors.First() as Error).Should().NotBeNull();
+			(payloadSender.Errors.First() as Error)?.Exception.Stacktrace.Should().Contain(m => m.Function == nameof(ClassWithAsync.TestMethodAsync));
+		}
+
+
+		/// <summary>
+		/// Makes sure that if a non-async method is named 'MoveNext', it does not cause any trouble
+		/// </summary>
+		[Fact]
+		public void CallStackWithMoveNextWithoutAsync()
+		{
+			var payloadSender = new MockPayloadSender();
+			var agent = new ApmAgent(new TestAgentComponents(payloadSender: payloadSender));
+
+			Assert.Throws<Exception>(() =>
+			{
+				agent.Tracer.CaptureTransaction("TestTransaction", "Test", () =>
+				{
+					var classWithSyncMethods = new ClassWithSyncMethods();
+					classWithSyncMethods.MoveNext();
+				});
+			});
+
+			payloadSender.Errors.Should().NotBeEmpty();
+			(payloadSender.Errors.First() as Error).Should().NotBeNull();
+			(payloadSender.Errors.First() as Error)?.Exception.Stacktrace.Should().Contain(m => m.Function == nameof(ClassWithSyncMethods.MoveNext));
+			(payloadSender.Errors.First() as Error)?.Exception.Stacktrace.Should().Contain(m => m.Function == nameof(ClassWithSyncMethods.M2));
+		}
+
+		private class ClassWithSyncMethods
+		{
+			[MethodImpl(MethodImplOptions.NoInlining)]
+			internal void MoveNext() => M2();
+
+			[MethodImpl(MethodImplOptions.NoInlining)]
+			internal void M2() => throw new Exception("bamm");
+		}
+
+		private class ClassWithAsync
+		{
+			internal async Task TestMethodAsync()
+			{
+				await Task.Delay(5);
+				throw new Exception("bamm");
+			}
 		}
 	}
 }
