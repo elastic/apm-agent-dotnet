95d478ce54cdf33c968388743d033456953f3a82
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2019-06-12 21:59:35 +0300

    Fix build errors - Elastic.Apm.AspNetFullFramework.Tests
    Fix build errors - Elastic.Apm.AspNetFullFramework.Tests should be built only on Windows+Full Framework

--- .ci/windows/dotnet.bat
+++ .ci/windows/dotnet.bat
@@ -3,5 +3,6 @@
 ::
 dotnet sln remove sample/AspNetFullFrameworkSampleApp/AspNetFullFrameworkSampleApp.csproj
 dotnet sln remove src/Elastic.Apm.AspNetFullFramework/Elastic.Apm.AspNetFullFramework.csproj
+dotnet sln remove test/Elastic.Apm.AspNetFullFramework.Tests/Elastic.Apm.AspNetFullFramework.Tests.csproj 
 
 dotnet build
