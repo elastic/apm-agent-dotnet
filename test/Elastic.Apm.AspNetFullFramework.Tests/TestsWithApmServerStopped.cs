48ddc570981596f0a9f969c28764e6cdd7945ffa
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2019-06-13 10:01:40 +0300

    When sample response status is not as expected log response body

--- test/Elastic.Apm.AspNetFullFramework.Tests/TestsWithApmServerStopped.cs
+++ test/Elastic.Apm.AspNetFullFramework.Tests/TestsWithApmServerStopped.cs
@@ -1,18 +1,17 @@
 using System.Threading.Tasks;
-using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Elastic.Apm.AspNetFullFramework.Tests
 {
 	[Collection("AspNetFullFrameworkTests")]
-	public class TestsWithApmServerStopped : AspNetFullFrameworkTestsBase
+	public class TestsWithApmServerStopped : TestsBase
 	{
 		public TestsWithApmServerStopped(ITestOutputHelper xUnitOutputHelper) : base(xUnitOutputHelper, false) { }
 
 		[AspNetFullFrameworkTheory]
 		[MemberData(nameof(GenerateSampleAppUrlPathsData))]
 		public async Task SampleAppShouldBeAvailableEvenWhenApmServerStopped(SampleAppUrlPathData sampleAppUrlPathData) =>
-			(await SendGetRequestToSampleApp(sampleAppUrlPathData.UrlPath)).StatusCode.Should().Be(sampleAppUrlPathData.Status);
+			await SendGetRequestToSampleAppAndVerifyResponseStatusCode(sampleAppUrlPathData.UrlPath, sampleAppUrlPathData.Status);
 	}
 }
