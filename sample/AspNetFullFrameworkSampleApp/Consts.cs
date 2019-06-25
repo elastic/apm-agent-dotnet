67468af064ef9e45235daa97dd14463d39efd9d2
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2019-06-25 14:47:15 +0300

    Add APM-Server-URL to sample response header
    So that unit tests can verify that sample app runs with the correct environment variables

---
+++ sample/AspNetFullFrameworkSampleApp/Consts.cs
@@ -0,0 +1,8 @@
+namespace AspNetFullFrameworkSampleApp
+{
+	public class Consts
+	{
+		public const string ElasticApmServerUrlsResponseHeaderName = "Elastic-APM-Server-URLs";
+		public const string ProcessIdResponseHeaderName = "Process-ID";
+	}
+}
--- sample/AspNetFullFrameworkSampleApp/Global.asax.cs
+++ sample/AspNetFullFrameworkSampleApp/Global.asax.cs
@@ -1,7 +1,12 @@
-﻿using System.Web;
+﻿using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Elastic.Apm;
+using NLog;
 
 namespace AspNetFullFrameworkSampleApp
 {
@@ -10,10 +15,36 @@ namespace AspNetFullFrameworkSampleApp
 		protected void Application_Start()
 		{
 			LoggingConfig.SetupLogging();
+
+			var Logger = LogManager.GetCurrentClassLogger();
+			Logger.Info("Current process ID: {ProcessID}, ELASTIC_APM_SERVER_URLS: {ELASTIC_APM_SERVER_URLS}",
+				Process.GetCurrentProcess().Id, Environment.GetEnvironmentVariable("ELASTIC_APM_SERVER_URLS"));
+
 			AreaRegistration.RegisterAllAreas();
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
+		}
+
+		protected void Application_BeginRequest(object sender, EventArgs e)
+		{
+			if (Response.HeadersWritten) return;
+
+			Response.AddOnSendingHeaders((httpContext) =>
+			{
+				var responseHeaders = httpContext?.Response?.Headers;
+				if (responseHeaders == null) return;
+
+				AddHeaderIfNotPresent(responseHeaders, Consts.ElasticApmServerUrlsResponseHeaderName, string.Join(", ", Agent.Config.ServerUrls));
+				AddHeaderIfNotPresent(responseHeaders, Consts.ProcessIdResponseHeaderName, Process.GetCurrentProcess().Id.ToString());
+			});
+
+			void AddHeaderIfNotPresent(NameValueCollection responseHeaders, string headerName, string headerValue)
+			{
+				var alreadyExistingValues = responseHeaders.GetValues(headerName);
+				if (alreadyExistingValues != null && alreadyExistingValues.Length > 0) return;
+				responseHeaders.Add(headerName, headerValue);
+			}
 		}
 	}
 }
