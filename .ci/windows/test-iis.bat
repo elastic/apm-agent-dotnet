da0bab437e9bb40e70267fd7e1a964cd46be9b6b
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2019-06-25 20:58:44 +0300

    Make sure sample app log file is accessible to everyone

--- .ci/windows/test-iis.bat
+++ .ci/windows/test-iis.bat
@@ -1,4 +1,10 @@
 set ELASTIC_APM_TESTS_FULL_FRAMEWORK_ENABLED=true
+
+set sample_app_log_dir=C:\Elastic_APM_TEMP
+MKDIR %sample_app_log_dir%
+icacls %sample_app_log_dir% /t /grant Everyone:F
+set ELASTIC_APM_ASP_NET_FULL_FRAMEWORK_SAMPLE_APP_LOG_FILE=%sample_app_log_dir%\Elastic.Apm.AspNetFullFramework.Tests.SampleApp.log
+
 nuget restore ElasticApmAgent.sln
 msbuild
 dotnet test test\Elastic.Apm.AspNetFullFramework.Tests -v n -r target -d target\diag.log --no-build 
