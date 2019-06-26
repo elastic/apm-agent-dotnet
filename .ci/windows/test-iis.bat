3553ad9e7c4d7c4d094b03b5183c443ddc9d036c
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2019-06-26 09:27:36 +0300

    Call mkdir only when necessary to avoid "already exists" error message

--- .ci/windows/test-iis.bat
+++ .ci/windows/test-iis.bat
@@ -1,7 +1,7 @@
 set ELASTIC_APM_TESTS_FULL_FRAMEWORK_ENABLED=true
 
 set sample_app_log_dir=C:\Elastic_APM_TEMP
-MKDIR %sample_app_log_dir%
+if not exist "%sample_app_log_dir%" mkdir "%sample_app_log_dir%"
 icacls %sample_app_log_dir% /t /grant Everyone:F
 set ELASTIC_APM_ASP_NET_FULL_FRAMEWORK_SAMPLE_APP_LOG_FILE=%sample_app_log_dir%\Elastic.Apm.AspNetFullFramework.Tests.SampleApp.log
 
