ae2765cb4e95a1e8f763051d61966a9e6323c4e8
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2019-07-03 21:40:48 +0300

    Add test for DateTimeOffset,FormatForLog()

--- test/Elastic.Apm.Tests/HelpersTests/TimeExtensionsTests.cs
+++ test/Elastic.Apm.Tests/HelpersTests/TimeExtensionsTests.cs
@@ -42,7 +42,30 @@ namespace Elastic.Apm.Tests.HelpersTests
 		public void DateTimeFormatForLogTests(DateTime dateTime, string expectedFormattedForLog) =>
 			dateTime.FormatForLog().Should().Be(expectedFormattedForLog);
 
-		[Fact]
-		public void DateTimeOffsetFormatForLogTests() { }
+		public static IEnumerable<object[]> DateTimeOffsetFormattedForLogVariantsToTest()
+		{
+			yield return new object[]
+			{
+				new DateTimeOffset(new DateTime(1234, 5, 16, 17, 28, 39, DateTimeKind.Utc) + TimeUtils.TimeSpanFromFractionalMilliseconds(987.654)),
+				"1234-05-16 17:28:39.9876540 +00:00"
+			};
+
+			yield return new object[]
+			{
+				new DateTimeOffset(new DateTime(1987, 12, 31, 4, 5, 6, DateTimeKind.Utc) + TimeSpan.FromMilliseconds(123)),
+				"1987-12-31 04:05:06.1230000 +00:00"
+			};
+
+			yield return new object[]
+			{
+				new DateTimeOffset(new DateTime(2020, 2, 29, 23, 59, 58, DateTimeKind.Utc) + TimeSpan.FromMilliseconds(7)),
+				"2020-02-29 23:59:58.0070000 +00:00"
+			};
+		}
+
+		[Theory]
+		[MemberData(nameof(DateTimeOffsetFormattedForLogVariantsToTest))]
+		public void DateTimeOffsetFormatForLogTests(DateTimeOffset dateTimeOffset, string expectedFormattedForLog) =>
+			dateTimeOffset.FormatForLog().Should().Be(expectedFormattedForLog);
 	}
 }
