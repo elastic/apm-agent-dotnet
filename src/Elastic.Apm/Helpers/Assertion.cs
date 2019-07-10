de58ac77ec92746854393f43ee49f6c0407a5d12
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2019-07-10 10:44:31 +0300

    Include explanation in assertion message

--- src/Elastic.Apm/Helpers/Assertion.cs
+++ src/Elastic.Apm/Helpers/Assertion.cs
@@ -1,5 +1,11 @@
 namespace Elastic.Apm.Helpers
 {
+	/// <summary>
+	/// Assertion.IfEnabled?.That(...) is a replacement for System.Diagnostics.Contracts.Contract.Assert for agent's internal use -
+	/// for us to catch bugs in agent's code. It might be problematic to use Contract.Assert because application might have it disabled or
+	/// configured to do some strange things (pop GUI dialog boxes, etc.) so we need a separate runtime assertion facility
+	/// that we can control independently of whatever monitored application uses.
+	/// </summary>
 	internal static class Assertion
 	{
 		private static readonly AssertIfEnabled SingletonAssertIfEnabled = new AssertIfEnabled();
