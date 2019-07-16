fb660fa090951f652e866c8cf60ddd9ac1bf539e
Greg Kalapos <gergo@kalapos.net>
2019-07-16 17:38:48 +0200

    Update test/Elastic.Apm.Tests/HttpDiagnosticListenerTest.cs

--- test/Elastic.Apm.Tests/HttpDiagnosticListenerTest.cs
+++ test/Elastic.Apm.Tests/HttpDiagnosticListenerTest.cs
@@ -578,7 +578,7 @@ namespace Elastic.Apm.Tests
 		internal static (IDisposable, MockPayloadSender, ApmAgent) RegisterListenerAndStartTransaction(IApmLogger customLogger = null)
 		{
 			var payloadSender = new MockPayloadSender();
-			var agent = new ApmAgent(new TestAgentComponents(payloadSender: payloadSender));
+			var agent = new ApmAgent(new TestAgentComponents(payloadSender: payloadSender, stackTraceLimit: "-1"));
 			var sub = agent.Subscribe(new HttpDiagnosticsSubscriber());
 			StartTransaction(agent);
 
