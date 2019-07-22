4bd48a6b45ddebd445c890497df740b315995862
rocher <rocher@126.com>
2019-07-22 14:11:50 +0800

    dropping bootstrap and all the UI related things from the sample app.

--- sample/SmartSqlAspNetCodeApp/Controllers/HomeController.cs
+++ sample/SmartSqlAspNetCodeApp/Controllers/HomeController.cs
@@ -26,16 +26,5 @@ namespace SmartSqlAspNetCodeApp.Controllers
 			});
 			return View();
 		}
-
-		public IActionResult Privacy()
-		{
-			return View();
-		}
-
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
 	}
 }
