374d91dc67de8fed27a357689a9e4a5f7993a478
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-07-25 16:10:18 +0200

    fix: execute a nuget command after install

--- .ci/windows/tools.ps1
+++ .ci/windows/tools.ps1
@@ -4,14 +4,16 @@
 
 # Download .Net SDK installer script
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest "https://dot.net/v1/dotnet-install.ps1" -OutFile dotnet-install.ps1 -UseBasicParsing ;
+Invoke-RestMethod "https://dot.net/v1/dotnet-install.ps1" -OutFile dotnet-install.ps1 ;
 
 # Install .Net SDK'
 & ./dotnet-install.ps1 -Channel LTS -InstallDir ./dotnet -Version 2.1.505
 
 # Install NuGet Tool
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/v5.1.0/nuget.exe" -OutFile dotnet\\nuget.exe -UseBasicParsing ;
+Invoke-RestMethod "https://dist.nuget.org/win-x86-commandline/v5.1.0/nuget.exe" -OutFile dotnet\\nuget.exe ;
+
+& ./dotnet/nuget locals all -list
 
 # Install IIS
 Install-WindowsFeature -Name Web-Server, Web-Mgmt-Tools ;
