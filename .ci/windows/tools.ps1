251d01aef32d2a057b002b21526e5e7cabf0e1c4
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-07-25 15:19:44 +0200

    feat: bump nuget version, install IIS

--- .ci/windows/tools.ps1
+++ .ci/windows/tools.ps1
@@ -11,4 +11,7 @@ Invoke-WebRequest "https://dot.net/v1/dotnet-install.ps1" -OutFile dotnet-instal
 
 # Install NuGet Tool
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/v4.9.4/nuget.exe" -OutFile dotnet\\nuget.exe -UseBasicParsing ;
+Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/v5.1.0/nuget.exe" -OutFile dotnet\\nuget.exe -UseBasicParsing ;
+
+# Install IIS
+Install-WindowsFeature -Name Web-Server, Web-Mgmt-Tools ;
