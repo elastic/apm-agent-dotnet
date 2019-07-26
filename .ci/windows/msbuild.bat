81d7cebaf6b24c87244c99a1ed86ec1e8c853646
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-07-26 19:11:42 +0200

    fix: execute a nuget command after install

--- .ci/windows/msbuild.bat
+++ .ci/windows/msbuild.bat
@@ -6,7 +6,7 @@
 ::vswhere -latest -requires Microsoft.Build.NuGetSdkResolver
 ::vswhere -latest -requires Microsoft.Component.MSBuild -find **MSBuild\**\Bin\MSBuild.exe`
 
-nuget restore ElasticApmAgent.sln -verbosity detailed
-nuget update ElasticApmAgent.sln -verbosity detailed -MSBuildPath "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
+nuget restore ElasticApmAgent.sln -verbosity detailed -NonInteractive
+nuget update ElasticApmAgent.sln -verbosity detailed -NonInteractive -MSBuildPath "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
 
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild"
--- .ci/windows/tools.ps1
+++ .ci/windows/tools.ps1
@@ -3,11 +3,11 @@
 #
 
 # Download .Net SDK installer script
-#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-#Invoke-RestMethod "https://dot.net/v1/dotnet-install.ps1" -OutFile dotnet-install.ps1 ;
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-RestMethod "https://dot.net/v1/dotnet-install.ps1" -OutFile dotnet-install.ps1 ;
 
 # Install .Net SDK'
-#& ./dotnet-install.ps1 -Channel LTS -InstallDir ./dotnet -Version 2.1.505
+& ./dotnet-install.ps1 -Channel LTS -InstallDir ./dotnet -Version 2.1.505
 
 # Install NuGet Tool
 #[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -23,7 +23,7 @@ Install-WindowsFeature -Name Web-Server, Web-Mgmt-Tools ;
 #Invoke-RestMethod "https://github.com/microsoft/vswhere/releases/download/2.6.13%2Ba6d40ba5f4/vswhere.exe" -OutFile dotnet\\vswhere.exe ;
 
 #& choco install microsoft-build-tools -y --no-progress -r 
-& choco install nugetpackageexplorer -y --no-progress -r
-& choco install dotnetcore-sdk -y --no-progress -r --version 2.1.505
+#& choco install nugetpackageexplorer -y --no-progress -r
+#& choco install dotnetcore-sdk -y --no-progress -r --version 2.1.505
 & choco install nuget.commandline -y --no-progress -r --version 4.6.2
 & choco install vswhere -y --no-progress -r 
