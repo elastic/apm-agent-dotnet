f64ac279f64a9a21d543f088973166260e9e824c
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-07-26 18:05:08 +0200

    fix: execute a nuget command after install

--- .ci/windows/tools.ps1
+++ .ci/windows/tools.ps1
@@ -3,24 +3,27 @@
 #
 
 # Download .Net SDK installer script
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-RestMethod "https://dot.net/v1/dotnet-install.ps1" -OutFile dotnet-install.ps1 ;
+#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#Invoke-RestMethod "https://dot.net/v1/dotnet-install.ps1" -OutFile dotnet-install.ps1 ;
 
 # Install .Net SDK'
-& ./dotnet-install.ps1 -Channel LTS -InstallDir ./dotnet -Version 2.1.505
+#& ./dotnet-install.ps1 -Channel LTS -InstallDir ./dotnet -Version 2.1.505
 
 # Install NuGet Tool
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-RestMethod "https://dist.nuget.org/win-x86-commandline/v4.9.4/nuget.exe" -OutFile dotnet\\nuget.exe ;
+#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#Invoke-RestMethod "https://dist.nuget.org/win-x86-commandline/v4.9.4/nuget.exe" -OutFile dotnet\\nuget.exe ;
 
-& ./dotnet/nuget help
+#& ./dotnet/nuget help
 
 # Install IIS
 Install-WindowsFeature -Name Web-Server, Web-Mgmt-Tools ;
 
 # Install vswhere
+#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#Invoke-RestMethod "https://github.com/microsoft/vswhere/releases/download/2.6.13%2Ba6d40ba5f4/vswhere.exe" -OutFile dotnet\\vswhere.exe ;
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-RestMethod "https://github.com/microsoft/vswhere/releases/download/2.6.13%2Ba6d40ba5f4/vswhere.exe" -OutFile dotnet\\vswhere.exe ;
-
-choco install microsoft-build-tools
+#& choco install microsoft-build-tools
+& choco install nugetpackageexplorer
+& choco install dotnetcore-sdk
+& choco install nuget.commandline
+& choco install vswhere
