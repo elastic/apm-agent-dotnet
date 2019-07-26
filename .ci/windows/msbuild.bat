3aa429d0eb2cf99435f257c4e7acab65d66eafb8
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-07-26 12:30:20 +0200

    fix: execute a nuget command after install

--- .ci/windows/msbuild.bat
+++ .ci/windows/msbuild.bat
@@ -1,7 +1,7 @@
 ::
 :: This script runs the msbuild
 ::
-
+vswhere -products *
 vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`
 
 nuget update -self -verbosity detailed
