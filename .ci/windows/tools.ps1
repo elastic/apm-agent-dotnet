f3f903f8abd3d4899bdfe96af87a0fbb9d21e943
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-07-26 17:30:57 +0200

    fix: execute a nuget command after install

--- .ci/windows/tools.ps1
+++ .ci/windows/tools.ps1
@@ -9,17 +9,11 @@ Invoke-RestMethod "https://dot.net/v1/dotnet-install.ps1" -OutFile dotnet-instal
 # Install .Net SDK'
 & ./dotnet-install.ps1 -Channel LTS -InstallDir ./dotnet -Version 2.1.505
 
-./dotnet/dotnet add package Microsoft.Build.NuGetSdkResolver --version 4.9.4 --source https://dotnet.myget.org/F/nuget-build/api/v3/index.json
-
 # Install NuGet Tool
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-RestMethod "https://dist.nuget.org/win-x86-commandline/v4.9.4/nuget.exe" -OutFile dotnet\\nuget.exe ;
 
 & ./dotnet/nuget help
-
-Import-Module PowerShellGet
-Register-PSRepository -Name "nuget-build" -SourceLocation "https://dotnet.myget.org/F/nuget-build/api/v2"
-Install-Module -Name "Microsoft.Build.NuGetSdkResolver" -RequiredVersion "4.9.4-rtm.5839" -Repository "nuget-build" -AllowPreRelease
 
 # Install IIS
 Install-WindowsFeature -Name Web-Server, Web-Mgmt-Tools ;
@@ -29,4 +23,4 @@ Install-WindowsFeature -Name Web-Server, Web-Mgmt-Tools ;
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-RestMethod "https://github.com/microsoft/vswhere/releases/download/2.6.13%2Ba6d40ba5f4/vswhere.exe" -OutFile dotnet\\vswhere.exe ;
 
-choco install Microsoft.Build.NuGetSdkResolver --version 4.9.4 --source https://dotnet.myget.org/F/nuget-build/api/v2
+choco install microsoft-build-tools
