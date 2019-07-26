59ba14df1eb2ae354e1b92798c0c1e212d78f744
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-07-26 19:29:11 +0200

    fix: execute a nuget command after install

--- .ci/windows/msbuild.bat
+++ .ci/windows/msbuild.bat
@@ -7,8 +7,8 @@
 ::vswhere -latest -requires Microsoft.Component.MSBuild -find **MSBuild\**\Bin\MSBuild.exe`
 
 ::nuget restore ElasticApmAgent.sln -verbosity detailed -NonInteractive
-nuget restore ElasticApmAgent.sln -MSBuildPath $(Split-Path -Path (get-command msbuild.exe).source -Parent)
+nuget restore ElasticApmAgent.sln -verbosity detailed -NonInteractive -MSBuildPath "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
 
-nuget update ElasticApmAgent.sln -verbosity detailed -NonInteractive -MSBuildPath $(Split-Path -Path (get-command msbuild.exe).source -Parent)
+nuget update ElasticApmAgent.sln -verbosity detailed -NonInteractive -MSBuildPath "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
 
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild"
