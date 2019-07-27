41109b31ea2b27762aa7a6af8270a95c77ad6505
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-07-27 14:51:30 +0200

    fix: execute a nuget command after install

--- .ci/windows/msbuild.bat
+++ .ci/windows/msbuild.bat
@@ -3,11 +3,10 @@
 ::
 ::vswhere -products *
 ::vswhere -legacy
-::vswhere -latest -requires Microsoft.Build.NuGetSdkResolver
-::vswhere -latest -requires Microsoft.Component.MSBuild -find **MSBuild\**\Bin\MSBuild.exe`
+dotnet\vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`
+dotnet\vswhere -latest -requires Microsoft.Component.MSBuild -property installationPath
 
-nuget restore ElasticApmAgent.sln -verbosity detailed -NonInteractive
-::nuget restore ElasticApmAgent.sln -verbosity detailed -NonInteractive -MSBuildPath "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
-::nuget update ElasticApmAgent.sln -verbosity detailed -NonInteractive -MSBuildPath "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
+dotnet\nuget restore ElasticApmAgent.sln -verbosity detailed -NonInteractive -MSBuildPath "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
+dotnet\nuget update ElasticApmAgent.sln -verbosity detailed -NonInteractive -MSBuildPath "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
 
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild"
--- .ci/windows/tools.ps1
+++ .ci/windows/tools.ps1
@@ -6,7 +6,7 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-RestMethod "https://dot.net/v1/dotnet-install.ps1" -OutFile dotnet-install.ps1 ;
 
-## Install .Net SDK'
+## Install .Net SDK' - 2.1.801 2.2.401
 & ./dotnet-install.ps1 -Channel LTS -InstallDir ./dotnet -Version 2.1.505
 
 # Install NuGet Tool
@@ -19,11 +19,11 @@ Invoke-RestMethod "https://dist.nuget.org/win-x86-commandline/v5.1.0/nuget.exe" 
 Install-WindowsFeature -Name Web-Server, Web-Mgmt-Tools ;
 
 # Install vswhere
-#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-#Invoke-RestMethod "https://github.com/microsoft/vswhere/releases/download/2.6.13%2Ba6d40ba5f4/vswhere.exe" -OutFile dotnet\\vswhere.exe ;
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-RestMethod "https://github.com/microsoft/vswhere/releases/download/2.6.13%2Ba6d40ba5f4/vswhere.exe" -OutFile dotnet\\vswhere.exe ;
 
 #& choco install microsoft-build-tools -y --no-progress -r 
 #& choco install nugetpackageexplorer -y --no-progress -r
 #& choco install dotnetcore-sdk -y --no-progress -r --version 2.1.505
 #& choco install nuget.commandline -y --no-progress -r --version 4.9.4
-& choco install vswhere -y --no-progress -r 
+#& choco install vswhere -y --no-progress -r 
