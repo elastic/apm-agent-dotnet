57ca74086493f840c41c5082d42920a3ca9f692d
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-07-27 12:31:40 +0200

    fix: execute a nuget command after install

--- .ci/windows/dotnet.bat
+++ .ci/windows/dotnet.bat
@@ -5,6 +5,4 @@ dotnet sln remove sample/AspNetFullFrameworkSampleApp/AspNetFullFrameworkSampleA
 dotnet sln remove src/Elastic.Apm.AspNetFullFramework/Elastic.Apm.AspNetFullFramework.csproj
 dotnet sln remove test/Elastic.Apm.AspNetFullFramework.Tests/Elastic.Apm.AspNetFullFramework.Tests.csproj 
 
-dotnet sln remove src/Elastic.Apm.EntityFrameworkCore/Elastic.Apm.EntityFrameworkCore.csproj
-
-dotnet build --verbosity detailed --force -c Release
+dotnet build --verbosity detailed --force -c Release -r win-x64
