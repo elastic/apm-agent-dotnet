a7dd33d8d15e7a8319a7e19f59719f7056da5907
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-07-27 14:36:52 +0200

    fix: execute a nuget command after install

--- .ci/windows/msbuild.bat
+++ .ci/windows/msbuild.bat
@@ -6,9 +6,8 @@
 ::vswhere -latest -requires Microsoft.Build.NuGetSdkResolver
 ::vswhere -latest -requires Microsoft.Component.MSBuild -find **MSBuild\**\Bin\MSBuild.exe`
 
-::nuget restore ElasticApmAgent.sln -verbosity detailed -NonInteractive
+nuget restore ElasticApmAgent.sln -verbosity detailed -NonInteractive
 ::nuget restore ElasticApmAgent.sln -verbosity detailed -NonInteractive -MSBuildPath "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
 ::nuget update ElasticApmAgent.sln -verbosity detailed -NonInteractive -MSBuildPath "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
 
-dotnet restore --verbosity detailed 
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild"
--- .ci/windows/tools.ps1
+++ .ci/windows/tools.ps1
@@ -3,15 +3,15 @@
 #
 
 # Download .Net SDK installer script
-#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-#Invoke-RestMethod "https://dot.net/v1/dotnet-install.ps1" -OutFile dotnet-install.ps1 ;
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-RestMethod "https://dot.net/v1/dotnet-install.ps1" -OutFile dotnet-install.ps1 ;
 
 ## Install .Net SDK'
-#& ./dotnet-install.ps1 -Channel LTS -InstallDir ./dotnet -Version 2.1.505
+& ./dotnet-install.ps1 -Channel LTS -InstallDir ./dotnet -Version 2.1.505
 
 # Install NuGet Tool
-#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-#Invoke-RestMethod "https://dist.nuget.org/win-x86-commandline/v4.9.4/nuget.exe" -OutFile dotnet\\nuget.exe ;
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-RestMethod "https://dist.nuget.org/win-x86-commandline/v5.1.0/nuget.exe" -OutFile dotnet\\nuget.exe ;
 
 #& ./dotnet/nuget help
 
@@ -24,6 +24,6 @@ Install-WindowsFeature -Name Web-Server, Web-Mgmt-Tools ;
 
 #& choco install microsoft-build-tools -y --no-progress -r 
 #& choco install nugetpackageexplorer -y --no-progress -r
-& choco install dotnetcore-sdk -y --no-progress -r --version 2.1.505
+#& choco install dotnetcore-sdk -y --no-progress -r --version 2.1.505
 #& choco install nuget.commandline -y --no-progress -r --version 4.9.4
 & choco install vswhere -y --no-progress -r 
