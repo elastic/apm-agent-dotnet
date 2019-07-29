cdeaac9dd26eb7987d9b0bd69d8ad039fd1ceec3
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-07-29 14:51:25 +0200

    test: test on immutable worker with IIs

--- .ci/windows/test-iis.bat
+++ .ci/windows/test-iis.bat
@@ -1,4 +1,8 @@
 set ELASTIC_APM_TESTS_FULL_FRAMEWORK_ENABLED=true
-nuget restore ElasticApmAgent.sln
-msbuild
+
+set MSBUILD_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
+nuget restore ElasticApmAgent.sln -verbosity detailed -NonInteractive -MSBuildPath "%MSBUILD_PATH%"
+nuget update ElasticApmAgent.sln -verbosity detailed -NonInteractive -MSBuildPath "%MSBUILD_PATH%"
+
+"%MSBUILD_PATH%\msbuild"
 dotnet test test\Elastic.Apm.AspNetFullFramework.Tests -v n -r target -d target\diag.log --no-build 
