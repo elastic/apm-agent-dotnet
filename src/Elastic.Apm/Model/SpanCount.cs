4caf7c678f069476d532ec029e06da6112a104ba
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2019-09-05 08:32:01 +0300

    Introduce total count to avoid race condition

--- src/Elastic.Apm/Model/SpanCount.cs
+++ src/Elastic.Apm/Model/SpanCount.cs
@@ -7,12 +7,15 @@ namespace Elastic.Apm.Model
 	{
 		private int _dropped;
 		private int _started;
+		private int _total;
 		public int Dropped => _dropped;
 		public int Started => _started;
 
 		public void IncrementStarted() => Interlocked.Increment(ref _started);
 
 		public void IncrementDropped() => Interlocked.Increment(ref _dropped);
+
+		public int IncrementTotal() => Interlocked.Increment(ref _total);
 
 		public override string ToString() =>
 			new ToStringBuilder(nameof(SpanCount)) { { nameof(Started), Started }, { nameof(Dropped), Dropped } }.ToString();
