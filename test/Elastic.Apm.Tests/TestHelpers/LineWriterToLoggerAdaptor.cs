0c53008823d97cd0c042f0cf7d6a6992e5a30db6
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2019-09-13 21:20:20 +0300

    Use Exception.Message and Exception.StackTrace instead of Exception.ToString()

--- test/Elastic.Apm.Tests/TestHelpers/LineWriterToLoggerAdaptor.cs
+++ test/Elastic.Apm.Tests/TestHelpers/LineWriterToLoggerAdaptor.cs
@@ -23,9 +23,9 @@ namespace Elastic.Apm.Tests.TestHelpers
 
 			var message = formatter(state, e);
 
-			var fullMessage = e == null
-				? $"[{dateTime.ToString("yyyy-MM-dd hh:mm:ss")}][{ConsoleLogger.LevelToString(level)}] - {message}"
-				: $"[{dateTime.ToString("yyyy-MM-dd hh:mm:ss")}][{ConsoleLogger.LevelToString(level)}] - {message}{Environment.NewLine}+-> Exception: {e}";
+			var fullMessage = $"[{dateTime.ToString("yyyy-MM-dd hh:mm:ss")}][{ConsoleLogger.LevelToString(level)}] - {message}";
+			if (e != null)
+				fullMessage += $"{Environment.NewLine}+-> Exception: {e.GetType().FullName}: {e.Message}{Environment.NewLine}{e.StackTrace}";
 
 			if (IsEnabled(level))
 				_lineWriter.WriteLine(fullMessage);
