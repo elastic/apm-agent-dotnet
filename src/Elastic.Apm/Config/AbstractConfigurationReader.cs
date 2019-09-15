c98f1dd3c6edba2e471d27d774c450943a69f17d
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2019-09-15 21:17:20 +0300

    Cache MaxBatchEventCount and MaxQueueEventCount

--- src/Elastic.Apm/Config/AbstractConfigurationReader.cs
+++ src/Elastic.Apm/Config/AbstractConfigurationReader.cs
@@ -15,6 +15,9 @@ namespace Elastic.Apm.Config
 	{
 		private readonly LazyContextualInit<IReadOnlyList<Uri>> _cachedServerUrls = new LazyContextualInit<IReadOnlyList<Uri>>();
 
+		private readonly LazyContextualInit<int> _cachedMaxBatchEventCount = new LazyContextualInit<int>();
+		private readonly LazyContextualInit<int> _cachedMaxQueueEventCount = new LazyContextualInit<int>();
+
 		protected AbstractConfigurationReader(IApmLogger logger) => ScopedLogger = logger?.Scoped(GetType().Name);
 
 		protected IApmLogger Logger => ScopedLogger;
@@ -272,10 +275,12 @@ namespace Elastic.Apm.Config
 		}
 
 		protected int ParseMaxBatchEventCount(ConfigurationKeyValue kv) =>
-			ParseMaxXyzEventCount(kv, DefaultValues.MaxBatchEventCount, "MaxBatchEventCount");
+			_cachedMaxBatchEventCount.IfNotInited?.InitOrGet(() => ParseMaxXyzEventCount(kv, DefaultValues.MaxBatchEventCount, "MaxBatchEventCount"))
+			?? _cachedMaxBatchEventCount.Value;
 
 		protected int ParseMaxQueueEventCount(ConfigurationKeyValue kv) =>
-			ParseMaxXyzEventCount(kv, DefaultValues.MaxQueueEventCount, "MaxQueueEventCount");
+			_cachedMaxQueueEventCount.IfNotInited?.InitOrGet(() => ParseMaxXyzEventCount(kv, DefaultValues.MaxQueueEventCount, "MaxQueueEventCount"))
+			?? _cachedMaxQueueEventCount.Value;
 
 		protected TimeSpan ParseFlushInterval(ConfigurationKeyValue kv) =>
 			ParsePositiveOrZeroTimeIntervalInMillisecondsImpl(kv, TimeSuffix.S, TimeSpan.FromMilliseconds(DefaultValues.FlushIntervalInMilliseconds),
