681e3c6ed227b13073e9edb5f9f3488cf6bc2999
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2019-09-24 15:03:10 +0300

    Comment out `nuget update` in msbuild.bat

--- .ci/windows/msbuild.bat
+++ .ci/windows/msbuild.bat
@@ -8,6 +8,6 @@
 
 set MSBUILD_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
 nuget restore ElasticApmAgent.sln -verbosity detailed -NonInteractive -MSBuildPath %MSBUILD_PATH%
-nuget update ElasticApmAgent.sln -verbosity detailed -NonInteractive -MSBuildPath %MSBUILD_PATH%
+rem ### nuget update ElasticApmAgent.sln -verbosity detailed -NonInteractive -MSBuildPath %MSBUILD_PATH%
 
 %MSBUILD_PATH%\msbuild
