f63c6ebaa0788e695e2a24152a4a765c1d587711
Vadim Hatsura <vadimgatsura@gmail.com>
2019-09-25 14:46:47 +0300

    move transaction max spans tests to transaction sampling

--- test/Elastic.Apm.Tests/TransactionSamplingTests.cs
+++ test/Elastic.Apm.Tests/TransactionSamplingTests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Elastic.Apm.Tests.Mocks;
 using FluentAssertions;
 using Xunit;
@@ -33,6 +34,83 @@ namespace Elastic.Apm.Tests
 				mockPayloadSender.FirstTransaction.SpanCount.Started.Should().Be(0);
 				mockPayloadSender.Spans.Should().BeEmpty();
 			}
+		}
+
+		[Fact]
+		public void SpansShouldNotBeSent_WhenTransactionMaxSpansIsEqualToZero()
+		{
+			// Arrange
+			var mockPayloadSender = new MockPayloadSender();
+			var mockConfig = new MockConfigSnapshot(transactionMaxSpans: "0");
+
+			// Act
+			using (var agent = new ApmAgent(new TestAgentComponents(config: mockConfig, payloadSender: mockPayloadSender)))
+			{
+				agent.Tracer.CaptureTransaction("test transaction name", "test transaction type",
+					transaction =>
+						transaction.CaptureSpan("test span name", "test span type", span => { })
+				);
+			}
+
+			// Assert
+			mockPayloadSender.Transactions.Count.Should().Be(1);
+			mockPayloadSender.Spans.Count.Should().Be(0);
+			mockPayloadSender.FirstTransaction.SpanCount.Dropped.Should().Be(1);
+		}
+
+		[Fact]
+		public void LimitedAmountOfSpansShouldBeSent_WhenTransactionMaxSpansIsPositiveNumber()
+		{
+			// Arrange
+			const int spansCount = 10;
+			const int maxSpansCount = 5;
+			var mockPayloadSender = new MockPayloadSender();
+			var mockConfig = new MockConfigSnapshot(transactionMaxSpans: maxSpansCount.ToString());
+
+			// Act
+			using (var agent = new ApmAgent(new TestAgentComponents(config: mockConfig, payloadSender: mockPayloadSender)))
+			{
+				agent.Tracer.CaptureTransaction("test transaction name", "test transaction type",
+					transaction =>
+					{
+						foreach (var iteration in Enumerable.Range(1, spansCount))
+						{
+							transaction.CaptureSpan($"test span name #{iteration}", "test span type", span => { });
+						}
+					});
+			}
+
+			// Assert
+			mockPayloadSender.Transactions.Count.Should().Be(1);
+			mockPayloadSender.Spans.Count.Should().Be(maxSpansCount);
+			mockPayloadSender.FirstTransaction.SpanCount.Dropped.Should().Be(spansCount - maxSpansCount);
+		}
+
+		[Fact]
+		public void AllSpansShouldBeSent_WhenTransactionMaxSpansIsEqualToMinusOne()
+		{
+			// Arrange
+			var mockPayloadSender = new MockPayloadSender();
+			var mockConfig = new MockConfigSnapshot(transactionMaxSpans: "-1");
+			var spansCount = 1000;
+
+			// Act
+			using (var agent = new ApmAgent(new TestAgentComponents(config: mockConfig, payloadSender: mockPayloadSender)))
+			{
+				agent.Tracer.CaptureTransaction("test transaction name", "test transaction type",
+					transaction =>
+					{
+						foreach (var iteration in Enumerable.Range(1, spansCount))
+						{
+							transaction.CaptureSpan($"test span name #{iteration}", "test span type", span => { });
+						}
+					});
+			}
+
+			// Assert
+			mockPayloadSender.Transactions.Count.Should().Be(1);
+			mockPayloadSender.Spans.Count.Should().Be(spansCount);
+			mockPayloadSender.FirstTransaction.SpanCount.Dropped.Should().Be(0);
 		}
 	}
 }
