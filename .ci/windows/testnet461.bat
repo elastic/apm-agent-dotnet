9730f15d96ff36fa81d73c2c2f41001a216e5b35
Greg Kalapos <gergo@kalapos.net>
2019-09-27 15:48:44 +0200

    Update testnet461.bat

--- .ci/windows/testnet461.bat
+++ .ci/windows/testnet461.bat
@@ -2,4 +2,4 @@
 :: This script runs the tests and stored them in an xml file defined in the
 :: LogFilePath property
 ::
-dotnet test -f net461 -v n -r target -d target\diag.log --no-build --logger:"xunit;LogFilePath=TestResults.xml" /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=target\Coverage\ /p:Exclude=\"[Elastic.Apm.Tests]*,[SampleAspNetCoreApp*]*,[xunit*]*\" /p:Threshold=0 /p:ThresholdType=branch /p:ThresholdStat=total
+dotnet test -f net461 -v n -r target -d target\diag.log --no-build --logger:"xunit;LogFilePath=TestResults.xml" /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=target\Coverage\ /p:Exclude=\"[Elastic.Apm.Tests]*,[xunit*]*\" /p:Threshold=0 /p:ThresholdType=branch /p:ThresholdStat=total
