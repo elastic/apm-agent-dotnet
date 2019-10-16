aca434f55a41b1d883a53f70aa599260203ec7c6
Vadim Hatsura <vadimgatsura@gmail.com>
2019-10-16 19:52:54 +0300

    set detectd_hostname property in system info

--- src/Elastic.Apm/Api/System.cs
+++ src/Elastic.Apm/Api/System.cs
@@ -1,4 +1,6 @@
 using Elastic.Apm.Helpers;
+using Elastic.Apm.Report.Serialization;
+using Newtonsoft.Json;
 
 namespace Elastic.Apm.Api
 {
@@ -6,6 +8,11 @@ namespace Elastic.Apm.Api
 	{
 		public Container Container { get; set; }
 
-		public override string ToString() => new ToStringBuilder(nameof(System)) { { "Container", Container } }.ToString();
+		[JsonProperty("detected_hostname")]
+		[JsonConverter(typeof(TrimmedStringJsonConverter))]
+		public string Hostname { get; set; }
+
+		public override string ToString() =>
+			new ToStringBuilder(nameof(System)) { { nameof(Container), Container }, { nameof(Hostname), Hostname } }.ToString();
 	}
 }
