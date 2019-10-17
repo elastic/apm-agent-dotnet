512f54bfa3c094d43d6387349cb3a6bceb4bbf1e
Greg Kalapos <gergo@kalapos.net>
2019-10-17 16:46:09 +0200

    Update release.sh

--- .ci/linux/release.sh
+++ .ci/linux/release.sh
@@ -5,9 +5,8 @@
 #
 set -euxo pipefail
 
-# Remove sample projects
+# Remove sample projects - and other we don't want to pack
 dotnet sln remove test/Elastic.Apm.PerfTests/Elastic.Apm.PerfTests.csproj
-dotnet sln remove src/Elastic.Apm.AspNetFullFramework/Elastic.Apm.AspNetFullFramework.csproj
 dotnet sln remove test/Elastic.Apm.AspNetFullFramework.Tests/Elastic.Apm.AspNetFullFramework.Tests.csproj
-
+dotnet sln remove sample/AspNetFullFrameworkSampleApp/AspNetFullFrameworkSampleApp.csproj
 dotnet pack -c Release
