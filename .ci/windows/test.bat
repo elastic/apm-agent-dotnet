f520327dea4be2de5741cd0ce7fd41ae91a32bfc
Gergely Kalapos <gergo@kalapos.net>
2019-11-18 21:20:48 +0100

    Revert "Update test.bat"
    This reverts commit bbb20730765ab99fda71f38c24ef020a8cff9be4.

--- .ci/windows/test.bat
+++ .ci/windows/test.bat
@@ -2,6 +2,4 @@
 :: This script runs the tests and stored them in an xml file defined in the
 :: LogFilePath property
 ::
-
-SET NUGET_PACKAGES="%HOMEDRIVE%%HOMEPATH%/.nuget/packages"
 dotnet test -v n -r target -d target\diag.log --no-build --logger:"xunit;LogFilePath=TestResults.xml" /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=target\Coverage\ /p:Exclude=\"[Elastic.Apm.Tests]*,[SampleAspNetCoreApp*]*,[xunit*]*\" /p:Threshold=0 /p:ThresholdType=branch /p:ThresholdStat=total
