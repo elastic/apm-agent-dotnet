5e444696308205dda3730230bd84055d28cddb30
Gergely Kalapos <gergo@kalapos.net>
2019-11-21 10:18:31 +0100

    Fix IsConfigured test (#618)
    * Update ConfigTests.cs
    
    * Add StaticAgentTests class to host static tests
    
    * Update test/Elastic.Apm.Tests/StaticTests.cs
    
    Co-Authored-By: Sergey Kleyman <SergeyKleyman@users.noreply.github.com>

---
+++ test/Elastic.Apm.Tests/StaticTests.cs
@@ -0,0 +1,25 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Elastic.Apm.Tests
+{
+	/// <summary>
+	/// These tests access the static <see cref="Agent"/> instance.
+	/// All other tests should have their own <see cref="ApmAgent"/> instance and not rely on anything static.
+	/// Tests accessing the static <see cref="Agent"/> instance cannot run in parallel with tests that also access the static instance.
+	/// </summary>
+	public class StaticAgentTests
+	{
+		/// <summary>
+		/// Makes sure Agent.IsConfigured only returns true after Setup is called.
+		/// </summary>
+		[Fact]
+		public void IsConfigured()
+		{
+			Agent.IsConfigured.Should().BeFalse();
+
+			Agent.Setup(new AgentComponents());
+			Agent.IsConfigured.Should().BeTrue();
+		}
+	}
+}
