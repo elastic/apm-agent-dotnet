c35975991c8148e3823fbb9bd0932c66ef778a29
Greg Kalapos <gergo@kalapos.net>
2019-11-25 20:06:49 +0100

    Add Elastic.Apm.SerilogEnricher

---
+++ src/Elastic.Apm.SerilogEnricher/SerilogEnricher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Elastic.Apm.SerilogEnricher
+{
+	public class SerilogEnricher : ILogEventEnricher
+	{
+		public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+		{
+			if (Agent.IsConfigured)
+			{
+				if (Agent.Tracer.CurrentSpan != null)
+				{
+					logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+						"span.id", Agent.Tracer.CurrentSpan.Id));
+				}
+				else if (Agent.Tracer.CurrentTransaction != null)
+				{
+					logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+						"transaction.id", Agent.Tracer.CurrentTransaction.Id));
+					logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+						"trace.id", Agent.Tracer.CurrentTransaction.TraceId));
+				}
+			}
+		}
+	}
+}
