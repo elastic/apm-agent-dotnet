bc1f603a0dcdaa19577f47063262d39501b36fe9
Greg Kalapos <gergo@kalapos.net>
2019-12-05 15:00:48 +0100

    Update .NET CLI for CI

--- .ci/linux/tools.sh
+++ .ci/linux/tools.sh
@@ -8,4 +8,4 @@ set -euxo pipefail
 
 # Install .Net SDK
 curl -O https://dot.net/v1/dotnet-install.sh
-/bin/bash ./dotnet-install.sh --install-dir ${HOME}/dotnet -version 2.1.505
+/bin/bash ./dotnet-install.sh --install-dir ${HOME}/dotnet -version 3.1.100
--- .ci/windows/tools.ps1
+++ .ci/windows/tools.ps1
@@ -9,7 +9,7 @@ Add-WindowsFeature NET-Framework-45-ASPNET ;
 Add-WindowsFeature Web-Asp-Net45 ;
 
 # Install .Net SDK'
-& choco install dotnetcore-sdk -m -y --no-progress -r --version 2.1.505
+& choco install dotnetcore-sdk -m -y --no-progress -r --version 3.1.100
 
 # Install NuGet Tool
 & choco install nuget.commandline -y --no-progress -r --version 5.1.0
