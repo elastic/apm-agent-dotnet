1b5831bc60946f3fbfd99ca9512987ea9d0a8c03
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-09 20:05:19 +0000

    ci(jenkins): let's install previous version as required for windows

--- .ci/windows/tools.ps1
+++ .ci/windows/tools.ps1
@@ -10,6 +10,8 @@ Add-WindowsFeature Web-Asp-Net45 ;
 
 # Install .Net SDKs
 & choco install dotnetcore-sdk -m -y --no-progress -r --version 3.1.100
+& Invoke-WebRequest https://dot.net/v1/dotnet-install.ps1 -OutFile 'dotnet-install.ps1'
+& ./dotnet-install.ps1 --install-dir $Env:DOTNET_ROOT -version 2.1.505
 
 # Install NuGet Tool
 & choco install nuget.commandline -y --no-progress -r --version 5.1.0
