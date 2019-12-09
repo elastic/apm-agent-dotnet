336ed14d8fed88fbb0669319acfe87eeb60d317f
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-09 19:54:34 +0000

    Revert "ci(jenkins): simplify the script as used to be"
    This reverts commit 9bc422fb76d540d67820c1bdfaac3e22355fe55b.

---
+++ .ci/linux/tools.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# This script installs the required tools to be used during the build
+# NOTE: HOME env variable is declared in the pipeline which it stores the tools
+# which are required and downloaded on the fly.
+#
+set -euxo pipefail
+
+# Install .Net SDK
+curl -O https://dot.net/v1/dotnet-install.sh -L
+
+chmod +x dotnet-install.sh
+
+source ./dotnet-install.sh -version 2.1.505
