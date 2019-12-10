7d5c7c84485f78f0040d92afa8a19ad45f2458bf
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-10 20:09:06 +0000

    install the workload tools

--- .ci/windows/msbuild-tools.ps1
+++ .ci/windows/msbuild-tools.ps1
@@ -3,4 +3,5 @@
 #
 
 # Install visualstudio2019buildtools
+& choco install visualstudio2017-workload-netcorebuildtools -m -y --no-progress -r --version=1.1.2
 & choco install visualstudio2019buildtools -m -y --no-progress -r --version=16.3.10.0
