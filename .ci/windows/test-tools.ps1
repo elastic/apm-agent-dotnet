dbba2b9a915fa439283cbc9525e54c5cd7314251
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-10 14:21:31 +0000

    ci(jenkins): as stated in the review and also solved with https://github.com/elastic/apm-agent-dotnet/pull/426 as it's not required in this particular scenario for windows

--- .ci/windows/test-tools.ps1
+++ .ci/windows/test-tools.ps1
@@ -2,10 +2,6 @@
 # This script installs the required test tools to be used during the tests execution
 #
 
-& dotnet sln remove sample/AspNetFullFrameworkSampleApp/AspNetFullFrameworkSampleApp.csproj
-& dotnet sln remove src/Elastic.Apm.AspNetFullFramework/Elastic.Apm.AspNetFullFramework.csproj
-& dotnet sln remove test/Elastic.Apm.AspNetFullFramework.Tests/Elastic.Apm.AspNetFullFramework.Tests.csproj
-
 # Install tools
 & dotnet tool install -g dotnet-xunit-to-junit --version 0.3.1
 & dotnet tool install -g Codecov.Tool --version 1.2.0
