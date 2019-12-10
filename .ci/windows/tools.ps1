e4144177a3b97ccefc3875a9110b14c1d9ee906d
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-10 15:18:04 +0000

    add 2.2 as requested for building The framework 'Microsoft.AspNetCore.App', version '2.2.0' was not found.

--- .ci/windows/tools.ps1
+++ .ci/windows/tools.ps1
@@ -10,6 +10,7 @@ Add-WindowsFeature Web-Asp-Net45 ;
 # Install .Net SDKs
 & choco install dotnetcore-sdk -m -y --no-progress -r --version 3.1.100
 & choco install dotnetcore-sdk -m -y --no-progress -r --version 2.1.505
+& choco install dotnetcore-sdk -m -y --no-progress -r --version 2.2.110
 
 # Install NuGet Tool
 & choco install nuget.commandline -y --no-progress -r --version 5.1.0
