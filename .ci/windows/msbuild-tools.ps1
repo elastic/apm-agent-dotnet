eca13ca084fb6dbf7fee0c39d82b6e768ac21c6d
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-11 11:41:19 +0000

    Revert "install the workload tools"
    This reverts commit 7d5c7c84485f78f0040d92afa8a19ad45f2458bf.

--- .ci/windows/msbuild-tools.ps1
+++ .ci/windows/msbuild-tools.ps1
@@ -3,5 +3,4 @@
 #
 
 # Install visualstudio2019buildtools
-& choco install visualstudio2017-workload-netcorebuildtools -m -y --no-progress -r --version=1.1.2
 & choco install visualstudio2019buildtools -m -y --no-progress -r --version=16.3.10.0
