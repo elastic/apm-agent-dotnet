247e2182ed0aee57839b3a92e5ccfd9d1c31ca07
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-11 18:15:20 +0000

    some echo to help with

--- .ci/windows/msbuild.bat
+++ .ci/windows/msbuild.bat
@@ -1,6 +1,7 @@
 ::
 :: This script runs the msbuild
 ::
-
+echo "Prepare context for VsDevCmd.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
 nuget restore -verbosity detailed -NonInteractive
 msbuild
