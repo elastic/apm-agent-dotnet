ad90e007e5876422ad22bc8d76215e2785a39d8e
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-11 20:40:09 +0000

    let's see if the sdks env variable is required

--- .ci/windows/msbuild.bat
+++ .ci/windows/msbuild.bat
@@ -2,6 +2,9 @@
 :: This script runs the msbuild
 ::
 echo "Prepare context for VsDevCmd.bat"
+set
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
+set
+set MSBuildSDKsPath="C:\Program Files\dotnet\sdk\3.1.100\Sdks"
 nuget restore -verbosity detailed -NonInteractive
 msbuild
