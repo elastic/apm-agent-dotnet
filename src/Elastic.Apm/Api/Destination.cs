209b2b6ad6a926ffba317358b2158a5d2d10632b
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2019-12-12 20:55:49 +0200

    Document Destination class.

--- src/Elastic.Apm/Api/Destination.cs
+++ src/Elastic.Apm/Api/Destination.cs
@@ -3,11 +3,22 @@ using Newtonsoft.Json;
 
 namespace Elastic.Apm.Api
 {
+	/// <summary>
+	/// Public API to set properties that are stored under <c>span.destination</c> in
+	/// <a href="https://www.elastic.co/guide/en/apm/get-started/current/transaction-spans.html">the APM data model</a>.
+	/// </summary>
 	public class Destination
 	{
 		private Optional<string> _address;
 		private Optional<int?> _port;
 
+		/// <summary>
+		/// Either an IP (v4 or v6) or a host/domain name.
+		/// See <a href="https://github.com/elastic/apm/issues/115#issuecomment-555814374">this issue</a> for more information.
+		/// If this property is not set via this public API it will be deduced from other parts of <see cref="SpanContext"/>
+		/// (for example <see cref="SpanContext.Http"/> or <see cref="SpanContext.Db"/>).
+		/// Explicitly setting this property to <c>null</c> will prohibit this automatic deduction.
+		/// </summary>
 		[JsonConverter(typeof(TrimmedStringJsonConverter))]
 		public string Address
 		{
@@ -15,6 +26,13 @@ namespace Elastic.Apm.Api
 			set => _address = new Optional<string>(value);
 		}
 
+		/// <summary>
+		/// Port number - it should not be omitted even if it's the default port number for the corresponding protocol.
+		/// See <a href="https://github.com/elastic/apm/issues/115#issuecomment-555814374">this issue</a> for more information.
+		/// If this property is not set via this public API it will be deduced from other parts of <see cref="SpanContext"/>
+		/// (for example <see cref="SpanContext.Http"/> or <see cref="SpanContext.Db"/>).
+		/// Explicitly setting this property to <c>null</c> will prohibit this automatic deduction.
+		/// </summary>
 		public int? Port
 		{
 			get => _port.Value;
