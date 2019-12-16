f58274e34e2f2a51684e266afc98806e51339f81
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-16 11:10:21 +0000

    ci(jenkins): let's build those samples

--- .ci/windows/prepare-test.bat
+++ .ci/windows/prepare-test.bat
@@ -1,6 +1,3 @@
 ::
 :: This script prepares the tests for the msbuild
 ::
-dotnet sln remove sample/AspNetFullFrameworkSampleApp/AspNetFullFrameworkSampleApp.csproj
-dotnet sln remove src/Elastic.Apm.AspNetFullFramework/Elastic.Apm.AspNetFullFramework.csproj
-dotnet sln remove test/Elastic.Apm.AspNetFullFramework.Tests/Elastic.Apm.AspNetFullFramework.Tests.csproj
