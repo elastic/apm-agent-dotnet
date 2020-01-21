1bfb7745d690476ad2774b7990fb053ab358ad71
Greg Kalapos <gergo@kalapos.net>
2020-01-21 10:55:37 +0100

    Update GcMetricsProvider.cs
    Collect clr.gc.genXsize for all heaps

--- src/Elastic.Apm/Metrics/MetricsProvider/GcMetricsProvider.cs
+++ src/Elastic.Apm/Metrics/MetricsProvider/GcMetricsProvider.cs
@@ -5,67 +5,100 @@ using Elastic.Apm.Api;
 
 namespace Elastic.Apm.Metrics.MetricsProvider
 {
-	internal class GcMetricsProvider : IMetricsProvider //TODO: Make disposable
+	/// <summary>
+	/// A metrics provider that collects GC metrics through EventSource
+	/// </summary>
+	internal class GcMetricsProvider : IMetricsProvider, IDisposable
 	{
-		private const string GcCount = "clr.gc.count";
-		private readonly SimpleEventListener _eventListener;
+		private const string GcCountName = "clr.gc.count";
+		private const string GcGen0SizeName = "clr.gc.gen0size";
+		private const string GcGen1SizeName = "clr.gc.gen1size";
+		private const string GcGen2SizeName = "clr.gc.gen2size";
+		private const string GcGen3SizeName = "clr.gc.gen3size";
 
-		private static readonly object Lock = new object();
-
-		public GcMetricsProvider() => _eventListener = new SimpleEventListener();
+		private readonly GcEventListener _eventListener = new GcEventListener();
 
 		public int ConsecutiveNumberOfFailedReads { get; set; }
 		public string DbgName => "GcMetricsProvider";
 
-		public IEnumerable<MetricSample> GetSamples()
+		public IEnumerable<MetricSample> GetSamples() => new List<MetricSample>
 		{
-			if (_eventListener.Samples == null)
-				return null;
+			new MetricSample(GcCountName, _eventListener.GcCount),
+			new MetricSample(GcGen0SizeName, _eventListener.Gen0Size),
+			new MetricSample(GcGen1SizeName, _eventListener.Gen1Size),
+			new MetricSample(GcGen2SizeName, _eventListener.Gen2Size),
+			new MetricSample(GcGen3SizeName, _eventListener.Gen3Size)
+		};
 
-			var retVal = new MetricSample[_eventListener.Samples.Count];
-			lock (Lock)
-			{
-				_eventListener.Samples.CopyTo(retVal, 0);
-				_eventListener.Samples.Clear();
-			}
+		public void Dispose() => _eventListener?.Dispose();
 
-			return retVal;
-		}
-
-		private class SimpleEventListener : EventListener
+		/// <summary>
+		/// An event listener that collects the GC stats
+		/// </summary>
+		private class GcEventListener : EventListener
 		{
-			private static readonly int keyword = 1; //TODO
+			private static readonly int keywordGC = 1; //TODO
 
-			internal List<MetricSample> Samples = new List<MetricSample>();
+			private EventSource _eventSourceDotNet;
 
-			public SimpleEventListener() =>
-				Console.WriteLine("SimpleEventListener ctor");
+			internal uint GcCount;
+			internal ulong Gen0Size;
+			internal ulong Gen1Size;
+			internal ulong Gen2Size;
+			internal ulong Gen3Size;
 
 			protected override void OnEventSourceCreated(EventSource eventSource)
 			{
-				Console.WriteLine(eventSource.Name);
 				if (!eventSource.Name.Equals("Microsoft-Windows-DotNETRuntime")) return;
 
-				EnableEvents(eventSource, EventLevel.Informational, (EventKeywords)keyword);
+				EnableEvents(eventSource, EventLevel.Informational, (EventKeywords)keywordGC);
+				_eventSourceDotNet = eventSource;
 			}
 
 			protected override void OnEventWritten(EventWrittenEventArgs eventData)
 			{
-				Console.WriteLine("OnEventWritten :" + eventData.EventName);
-
-				if (eventData.EventName.Contains("GCStart")) { } //TODO: remove
-
-				else if (eventData.EventName.Contains("GCEnd"))
+				// Collect heap sizes
+				if (eventData.EventName.Contains("GCHeapStats_V1"))
 				{
-					var indexOfCount = eventData.PayloadNames.IndexOf("Count");
+					SetValue("GenerationSize0", ref Gen0Size);
+					SetValue("GenerationSize1", ref Gen1Size);
+					SetValue("GenerationSize2", ref Gen2Size);
+					SetValue("GenerationSize3", ref Gen3Size);
+				}
+
+				// Collect GC count
+				if (eventData.EventName.Contains("GCEnd"))
+				{
+					var indexOfCount = IndexOf("Count");
 					if (indexOfCount < 0) return;
 
 					var gcCount = eventData.Payload[indexOfCount];
 
 					if (!(gcCount is uint gcCountInt)) return;
 
-					lock (Lock) Samples.Add(new MetricSample(GcCount, gcCountInt));
+					GcCount = gcCountInt;
 				}
+
+				void SetValue(string name, ref ulong value)
+				{
+					var gen0SizeIndex = IndexOf(name);
+					if (gen0SizeIndex < 0) return;
+
+					var gen0Size = eventData.Payload[gen0SizeIndex];
+					if (gen0Size is ulong gen0SizeLong)
+						value = gen0SizeLong;
+				}
+
+				int IndexOf(string name)
+				{
+					return eventData.PayloadNames.IndexOf(name);
+				}
+			}
+
+			public override void Dispose()
+			{
+				DisableEvents(_eventSourceDotNet);
+				base.Dispose();
 			}
 		}
 	}
