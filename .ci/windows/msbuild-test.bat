14d38fbfe2c6a6a816c1875b63dfa7413dac6d2f
William Penn <william.penn@elastic.co>
2020-02-04 10:16:14 -0500

    Adding nuget install cli refs

--- .ci/windows/msbuild-test.bat
+++ .ci/windows/msbuild-test.bat
@@ -1,8 +1,8 @@
-Install-Package dotnet-xunit-to-junit -Source c:\packages
-Install-Package Codecov.Tool -Source c:\packages
+nuget install dotnet-xunit-to-junit -Source c:\packages
+nuget install Codecov.Tool -Source c:\packages
 
 Get-ChildItem -Path . -Recurse -Filter *.csproj |
 Foreach-Object {
-  & Install-Package XunitXml.TestLogger -ProjectName $_.FullName -Source c:\packages
-  & Install-Package coverlet.msbuild -ProjectName $_.FullName -Source c:\packages
+  & nuget install XunitXml.TestLogger -ProjectName $_.FullName -Source c:\packages
+  & nuget install coverlet.msbuild -ProjectName $_.FullName -Source c:\packages
 }
