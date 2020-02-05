6ee6b119a5134936b5bec266dfe94efd86fecff6
dhtek <hick.damien@gmail.com>
2020-02-05 11:03:35 +0100

    Add soap action in the transaction name (#683)
    *  Retrieve soap action from headers and inject it in the transaction name
    
    * Fix minor changes

---
+++ sample/AspNetFullFrameworkSampleApp/Asmx/Health.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Health.asmx.cs" Class="AspNetFullFrameworkSampleApp.Asmx.Health" %>
---
+++ sample/AspNetFullFrameworkSampleApp/Asmx/Health.asmx.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Services;
+
+namespace AspNetFullFrameworkSampleApp.Asmx
+{
+	[WebService(Namespace = "http://tempuri.org/")]
+	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+	public class Health : WebService
+	{
+		[WebMethod]
+		public string Ping() => "Ok";
+	}
+}
