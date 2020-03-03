5f67c6f7a08af81c108099bc48ec0ae7f5140dfd
Victor Martinez <victormartinezrubio@gmail.com>
2020-03-03 16:28:26 +0000

    ci(jenkins): force the reinstall (#744)

--- .ci/windows/msbuild-tools.ps1
+++ .ci/windows/msbuild-tools.ps1
@@ -3,6 +3,6 @@
 #
 
 # Install visualstudio2019buildtools
-& choco install visualstudio2019buildtools -m -y --no-progress -r --version=16.4.0.0
-& choco install visualstudio2019enterprise -m -y --no-progress -r --version=16.4.0.0 --package-parameters "--includeRecommended --includeOptional"
-& choco install visualstudio2019-workload-netweb -m -y --no-progress -r --version=1.0.0
+& choco install visualstudio2019buildtools -m -y --no-progress --force -r --version=16.4.0.0
+& choco install visualstudio2019enterprise -m -y --no-progress --force -r --version=16.4.0.0 --package-parameters "--includeRecommended --includeOptional"
+& choco install visualstudio2019-workload-netweb -m -y --no-progress --force -r --version=1.0.0
