41894cf6cf3347ac826b536ce1918fb9f74ef400
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-10 15:52:51 +0000

    use latest

--- .ci/windows/msbuild-tools.ps1
+++ .ci/windows/msbuild-tools.ps1
@@ -3,6 +3,6 @@
 #
 
 # Install visualstudio2019buildtools
-& choco install visualstudio2019buildtools -m -y --no-progress -r --version=16.4.0.0
-& choco install visualstudio2019professional -m -y --no-progress -r --version=16.4.0.0 --package-parameters "--includeRecommended --includeOptional"
+& choco install visualstudio2019buildtools -m -y --force --no-progress -r
+& choco install visualstudio2019professional -m -y --force --no-progress -r --package-parameters "--includeRecommended --includeOptional"
 & choco install visualstudio2019-workload-netweb -m -y --no-progress -r --version=1.0.0
