935994eca972c8f1fa751c423bc3ce6fa8ba0d34
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-10 20:46:20 +0000

    force version 16.4.5.0

--- .ci/windows/msbuild-tools.ps1
+++ .ci/windows/msbuild-tools.ps1
@@ -3,6 +3,6 @@
 #
 
 # Install visualstudio2019buildtools
-& choco install visualstudio2019buildtools -m -y --force --no-progress -r
-& choco install visualstudio2019professional -m -y --force --no-progress -r --package-parameters "--includeRecommended --includeOptional"
+& choco install visualstudio2019buildtools -m -y --force --no-progress -r --version=16.4.5.0
+& choco install visualstudio2019professional -m -y --force --no-progress -r --version=16.4.5.0 --package-parameters "--includeRecommended --includeOptional"
 & choco install visualstudio2019-workload-netweb -m -y --no-progress -r --version=1.0.0
