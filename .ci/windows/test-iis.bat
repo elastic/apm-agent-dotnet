96b8e68cbceca4e618ad37e1cfac67157e03c6df
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-11 10:44:06 +0000

    ci(jenkins): filter VerifyMetricsBasicConstraints for the test IIS

--- .ci/windows/test-iis.bat
+++ .ci/windows/test-iis.bat
@@ -4,4 +4,4 @@ if not exist "%sample_app_log_dir%" mkdir "%sample_app_log_dir%"
 icacls %sample_app_log_dir% /t /grant Everyone:F
 set ELASTIC_APM_ASP_NET_FULL_FRAMEWORK_SAMPLE_APP_LOG_FILE=%sample_app_log_dir%\Elastic.Apm.AspNetFullFramework.Tests.SampleApp.log
 
-dotnet test test\Elastic.Apm.AspNetFullFramework.Tests -v n -r target -d target\diag.log --no-build
+dotnet test test\Elastic.Apm.AspNetFullFramework.Tests --filter VerifyMetricsBasicConstraints -v n -r target -d target\diag.log --no-build
