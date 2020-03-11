b1ce4f52f518785d04b752505b1ba5c76cdb200c
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-11 20:07:44 +0000

    revert the magic trap error ps1 function since it does nothing

--- .ci/windows/test-tools.ps1
+++ .ci/windows/test-tools.ps1
@@ -2,31 +2,11 @@
 # This script installs the required test tools to be used during the tests execution
 #
 
-function Exec {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $true)]
-        [scriptblock]$cmd,
-        [string]$errorMessage = ($msgs.error_bad_command -f $cmd)
-    )
-
-    try {
-        $global:lastexitcode = 0
-        & $cmd 2>&1 | %{ "$_" }
-        if ($lastexitcode -ne 0) {
-            throw $errorMessage
-        }
-    }
-    catch [Exception] {
-        throw $_
-    }
-}
-
 # Install tools
-exec { dotnet tool install -g Codecov.Tool --version 1.2.0 }
+& dotnet tool install -g Codecov.Tool --version 1.2.0
 
 Get-ChildItem -Path . -Recurse -Filter *.csproj | Where-Object { $_.Name -NotMatch "AspNetFullFrameworkSampleApp.csproj" } |
 Foreach-Object {
-	exec { dotnet add $_.FullName package JunitXml.TestLogger --version 2.1.15 }
-	exec { dotnet add $_.FullName package coverlet.msbuild --version 2.5.1 }
+	& dotnet add $_.FullName package JunitXml.TestLogger --version 2.1.15
+	& dotnet add $_.FullName package coverlet.msbuild --version 2.5.1
 }
