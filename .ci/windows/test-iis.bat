8085f1f0afc7ee041f6775b9d70536aa8c637922
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-11 13:03:29 +0000

    ci(jenkins): use junit library

--- .ci/windows/test-iis.bat
+++ .ci/windows/test-iis.bat
@@ -5,4 +5,4 @@ icacls %sample_app_log_dir% /t /grant Everyone:F
 set ELASTIC_APM_ASP_NET_FULL_FRAMEWORK_SAMPLE_APP_LOG_FILE=%sample_app_log_dir%\Elastic.Apm.AspNetFullFramework.Tests.SampleApp.log
 
 dotnet test test\Elastic.Apm.AspNetFullFramework.Tests -v n -r target -d target\diag.log --no-build ^
- --logger:"xunit;LogFilePath=TestResults.xml"
+ --logger:"junit;LogFilePath={framework}-{assembly}-test-result.xml;MethodFormat=Class;FailureBodyFormat=Verbose" ^
