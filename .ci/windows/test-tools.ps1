31f9197203dffc7b5fc7cbfa24c9cc6752935197
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-11 20:15:14 +0000

    Required to load the VsDevCmd to use some env variables

--- .ci/windows/test-tools.ps1
+++ .ci/windows/test-tools.ps1
@@ -2,6 +2,18 @@
 # This script installs the required test tools to be used during the tests execution
 #
 
+## https://stackoverflow.com/questions/2124753/how-can-i-use-powershell-with-the-visual-studio-command-prompt
+Write-Host "`nPrepare context for VsDevCmd.bat" -ForegroundColor Yellow
+pushd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools"
+cmd /c "VsDevCmd.bat&set" |
+foreach {
+  if ($_ -match "=") {
+    $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+  }
+}
+popd
+Write-Host "`nVisual Studio 2019 Command Prompt variables set." -ForegroundColor Yellow
+
 # Install tools
 & dotnet tool install -g Codecov.Tool --version 1.2.0
 
