7e7f01fbd9f7a31e4456bdcc72fc67544d089757
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-11 16:45:04 +0000

    Let's help with the ps1 debugging

--- .ci/windows/test-tools.ps1
+++ .ci/windows/test-tools.ps1
@@ -2,11 +2,31 @@
 # This script installs the required test tools to be used during the tests execution
 #
 
+function Exec {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [scriptblock]$cmd,
+        [string]$errorMessage = ($msgs.error_bad_command -f $cmd)
+    )
+
+    try {
+        $global:lastexitcode = 0
+        & $cmd 2>&1 | %{ "$_" }
+        if ($lastexitcode -ne 0) {
+            throw $errorMessage
+        }
+    }
+    catch [Exception] {
+        throw $_
+    }
+}
+
 # Install tools
-& dotnet tool install -g Codecov.Tool --version 1.2.0
+exec { dotnet tool install -g Codecov.Tool --version 1.2.0 }
 
 Get-ChildItem -Path . -Recurse -Filter *.csproj |
 Foreach-Object {
-  & dotnet add $_.FullName package JunitXml.TestLogger --version 2.1.15
-  & dotnet add $_.FullName package coverlet.msbuild --version 2.5.1
+	exec { dotnet add $_.FullName package JunitXml.TestLogger --version 2.1.15 }
+	exec { dotnet add $_.FullName package coverlet.msbuild --version 2.5.1 }
 }
