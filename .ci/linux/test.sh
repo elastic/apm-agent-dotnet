b334f02756d7673851fe31d64c3e4d7c46cad226
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-13 09:55:28 +0000

    ci(jenkins): coverlet support in Linux

--- .ci/linux/test.sh
+++ .ci/linux/test.sh
@@ -23,16 +23,16 @@ do
 		continue
 	fi
 	dotnet add "$i" package XunitXml.TestLogger --version 2.0.0
-	dotnet add "$i" package coverlet.msbuild --version 2.5.1
 done
 
 # Run tests
-dotnet test -v n -r target -d target/diag.log \
+dotnet test ElasticApmAgent.sln -v n -r target -d target/diag.log \
     --logger:"xunit;LogFilePath=TestResults.xml" \
+	--collect:"XPlat Code Coverage" \
+	--settings coverlet.runsettings \
     /p:CollectCoverage=true \
     /p:CoverletOutputFormat=cobertura \
     /p:CoverletOutput=target/Coverage/ \
-    /p:Exclude='"[Elastic.Apm.Tests]*,[SampleAspNetCoreApp*]*,[xunit*]*"' \
     /p:Threshold=0 \
     /p:ThresholdType=branch \
     /p:ThresholdStat=total \
