27ce3d10a23569dc9492bc7c3f897d68aa40cc35
Greg Kalapos <gergo@kalapos.net>
2020-03-16 22:02:26 +0100

    Remove choco install netfx-4.6.1-devpack

--- .ci/windows/testnet461.bat
+++ .ci/windows/testnet461.bat
@@ -4,6 +4,5 @@
 ::
 dotnet publish test\Elastic.Apm.Tests --framework net461 -o outtestnet461
 
-
 dotnet vstest outtestnet461\Elastic.Apm.Tests.dll ^
  --logger:"junit;LogFilePath=junit-{framework}-{assembly}.xml;MethodFormat=Class;FailureBodyFormat=Verbose"
