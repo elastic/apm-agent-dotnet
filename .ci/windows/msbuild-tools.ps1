cd13adcda1db2c6539ff255d7ca67dd715fa0a3f
Greg Kalapos <gergo@kalapos.net>
2020-03-16 19:46:15 +0100

    Change netfx-4.6.2 to 4.6.1 and move after VS install

--- .ci/windows/msbuild-tools.ps1
+++ .ci/windows/msbuild-tools.ps1
@@ -6,3 +6,6 @@
 & choco install visualstudio2019buildtools -m -y --no-progress --force -r --version=16.4.5.0
 & choco install visualstudio2019professional -m -y --no-progress --force -r --version=16.4.5.0 --package-parameters "--includeRecommended --includeOptional"
 & choco install visualstudio2019-workload-netweb -m -y --no-progress --force -r --version=1.0.0
+
+# Install .NET Framework 4.6.1
+& choco install netfx-4.6.1-devpack -y --force
