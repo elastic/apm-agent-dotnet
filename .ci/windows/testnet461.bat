d387d554b717858372aff48f4a252667e9651d3c
Gergely Kalapos <gergo@kalapos.net>
2020-03-16 17:24:34 +0000

    Update testnet461.bat

--- .ci/windows/testnet461.bat
+++ .ci/windows/testnet461.bat
@@ -2,7 +2,7 @@
 :: This script runs the tests and stored them in an xml file defined in the
 :: LogFilePath property
 ::
-dotnet test test\Elastic.Apm.Tests -f net461 -v n -r target -d target\diag.log --no-build ^
+dotnet test test\Elastic.Apm.Tests -f net461 -v n -r target -d target\diag.log ^
  --logger:"junit;LogFilePath=junit-{framework}-{assembly}.xml;MethodFormat=Class;FailureBodyFormat=Verbose" ^
  /p:CollectCoverage=true ^
  /p:CoverletOutputFormat=cobertura ^
