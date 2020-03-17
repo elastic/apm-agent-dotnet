1b9ea85c9f2fa78edcb49d354ed9b0d16a2e62d2
Greg Kalapos <gergo@kalapos.net>
2020-03-17 17:42:07 +0100

    Remove ContextLocalHolder<T>
    Not used anymore since logging was removed

--- src/Elastic.Apm/CurrentExecutionSegmentsContainer.cs
+++ src/Elastic.Apm/CurrentExecutionSegmentsContainer.cs
@@ -1,20 +1,12 @@
 using System.Threading;
-using Elastic.Apm.Helpers;
-using Elastic.Apm.Logging;
 using Elastic.Apm.Model;
 
 namespace Elastic.Apm
 {
 	internal sealed class CurrentExecutionSegmentsContainer : ICurrentExecutionSegmentsContainer
 	{
-		private readonly ContextLocalHolder<Span> _currentSpan;
-		private readonly ContextLocalHolder<Transaction> _currentTransaction;
-
-		internal CurrentExecutionSegmentsContainer(IApmLogger logger)
-		{
-			_currentTransaction = new ContextLocalHolder<Transaction>(logger, "transaction");
-			_currentSpan = new ContextLocalHolder<Span>(logger, "span");
-		}
+		private readonly AsyncLocal<Span> _currentSpan = new AsyncLocal<Span>();
+		private readonly AsyncLocal<Transaction> _currentTransaction = new AsyncLocal<Transaction>();
 
 		public Span CurrentSpan
 		{
@@ -26,21 +18,6 @@ namespace Elastic.Apm
 		{
 			get => _currentTransaction.Value;
 			set => _currentTransaction.Value = value;
-		}
-
-		private sealed class ContextLocalHolder<T>
-		{
-			private readonly IApmLogger _logger;
-			private readonly AsyncLocal<T> _value = new AsyncLocal<T>();
-
-			internal ContextLocalHolder(IApmLogger logger, string loggerNameSuffix) =>
-				_logger = logger.Scoped($"{nameof(CurrentExecutionSegmentsContainer)}.{loggerNameSuffix}");
-
-			internal T Value
-			{
-				get => _value.Value;
-				set => _value.Value = value;
-			}
 		}
 	}
 }
