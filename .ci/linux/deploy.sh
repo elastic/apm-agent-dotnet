2f0c6a0ea6917fdb63a6671616f75173b2922956
Greg Kalapos <gergo@kalapos.net>
2020-03-18 16:34:41 +0100

    Update deploy.sh
    Find all .nupkg file and then match those to a predefined array of strings that contain all packages we want to push.

--- .ci/linux/deploy.sh
+++ .ci/linux/deploy.sh
@@ -4,9 +4,17 @@
 #
 set -euo pipefail
 
-for nupkg in Elastic.Apm Elastic.Apm.AspNetCore Elastic.Apm.EntityFrameworkCore Elastic.Apm.NetCoreAll Elastic.Apm.EntityFramework6 Elastic.Apm.AspNetFullFramework
+declare -a projectsToPublish=("Elastic.Apm" "Elastic.Apm.AspNetCore" "Elastic.Apm.EntityFrameworkCore" "Elastic.Apm.NetCoreAll" "Elastic.Apm.EntityFramework6" "Elastic.Apm.AspNetFullFramework")
+
+for project in  "${projectsToPublish[@]}"
 do
-	nupkg+=".nupkg"
-	echo "dotnet nuget push ${nupkg}"
-	dotnet nuget push ${nupkg} -k ${1} -s ${2}
+	for nupkg in $(find . -type f -not -path './.nuget/*' -name '*.nupkg')
+	do
+		pattern=".*${project}[0-9|.]*.nupkg"
+		if [[ $nupkg =~ $pattern ]]
+		then
+			echo "dotnet nuget push ${nupkg}"
+			dotnet nuget push ${nupkg} -k ${1} -s ${2}
+		fi
+	done
 done
