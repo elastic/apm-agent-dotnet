2cf7e31b5c987115a398056e328ee40221439aa8
Greg Kalapos <gergo@kalapos.net>
2020-04-17 11:25:05 +0200

    Update Program.cs
    Change exporter settings based on env. vars

--- test/Elastic.Apm.PerfTests/Program.cs
+++ test/Elastic.Apm.PerfTests/Program.cs
@@ -1,4 +1,5 @@
-﻿using BenchmarkDotNet.Configs;
+﻿using System;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 using Elastic.CommonSchema.BenchmarkDotNetExporter;
 
@@ -8,7 +9,23 @@ namespace Elastic.Apm.PerfTests
 	{
 		public static void Main(string[] args)
 		{
-			var options = new ElasticsearchBenchmarkExporterOptions("http://localhost:9200");
+			var esUrl = Environment.GetEnvironmentVariable("ES_URL");
+			var esPassword = Environment.GetEnvironmentVariable("ES_PASS");
+			var esUser = Environment.GetEnvironmentVariable("ES_USER");
+
+			ElasticsearchBenchmarkExporterOptions options;
+
+			if (!string.IsNullOrEmpty(esUrl) && !string.IsNullOrEmpty(esPassword) && !string.IsNullOrEmpty(esUser))
+			{
+				Console.WriteLine($"Setting ElasticsearchBenchmarkExporterOptions based on environment variables - es URL: {esUrl}");
+				options = new ElasticsearchBenchmarkExporterOptions(esUrl) { Username = esUser, Password = esPassword };
+			}
+			else
+			{
+				Console.WriteLine(
+					"Setting ElasticsearchBenchmarkExporterOptions to export data to http://localhost:9200 - to change this set following environment variables: ES_URL, ES_PASS, ES_USER");
+				options = new ElasticsearchBenchmarkExporterOptions("http://localhost:9200");
+			}
 
 			var exporter = new ElasticsearchBenchmarkExporter(options);
 			var config = DefaultConfig.Instance.With(exporter);
