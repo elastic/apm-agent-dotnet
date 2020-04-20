9ebb71dec56590f70b9a7f97bbc278f274ee1070
Martijn Laarman <Mpdreamz@gmail.com>
2020-04-20 20:00:41 +0200

    Remove dependency on Elastic.Managed.Ephemeral
    Only used in example project, switched to one that talks to
    elastic/apm-integration-testing.

--- sample/ElasticsearchSample/Program.cs
+++ sample/ElasticsearchSample/Program.cs
@@ -5,72 +5,83 @@ using System.Threading.Tasks;
 using Elastic.Apm;
 using Elastic.Apm.Api;
 using Elastic.Apm.Elasticsearch;
-using Elastic.Managed.Ephemeral;
 using Elasticsearch.Net;
+using Microsoft.Extensions.Configuration;
 using Nest;
-using Context = Elastic.Apm.Api.Context;
 
 namespace ElasticsearchSample
 {
-	public static class Program
+	public class Program
 	{
-		public class MyDocument
+		private class MyDocument
 		{
 			public string Id { get; set; }
 		}
+
+		private static ConnectionSettings DefaultConnectionSettings(ConnectionSettings s) => s
+			.DefaultIndex("index")
+			.DisableDirectStreaming();
 
 		private static async Task Main(string[] args)
 		{
 			Agent.Subscribe(new ElasticsearchDiagnosticsSubscriber());
 
-			var clusterConfiguration = new EphemeralClusterConfiguration("7.2.0")
-			{
-				StartingPortNumber = 9202,
-				HttpFiddlerAware = true //Automatically routes to `ipv4.fiddler` on windows and
-				// linux if Fiddler or mitmproxy is running (make sure ipv4.fiddler exists in hosts on linux)
-			};
-			using (var cluster = new EphemeralCluster(clusterConfiguration))
-			{
-				//starts the cluster and waits 3 minutes for confirmation that it started
-				cluster.Start(TimeSpan.FromMinutes(3));
+			// using var cluster = CreateEphemeralClient(out var connectionPool);
+			var client = CreateLocalClient();
 
-				var connection = new SniffingConnectionPool(cluster.NodesUris());
-				var settings = new ConnectionSettings(connection)
-					.DefaultIndex("index")
-					.EnableDebugMode();
-				var client = new ElasticClient(settings);
+			// warm up
+			await Agent.Tracer.CaptureTransaction("Warmup", ApiConstants.TypeDb, async () =>
+				await client.SearchAsync<object>(new SearchRequest())
+			);
 
-				// warm up
-				await Agent.Tracer.CaptureTransaction("Warmup", ApiConstants.TypeDb, async () =>
+			client.IndexDocument(new MyDocument { Id = "1" });
+
+			for (var i = 0; i < 10; i++)
+			{
+				// async
+				await Agent.Tracer.CaptureTransaction("Async Call", ApiConstants.TypeDb, async () =>
 					await client.SearchAsync<object>(new SearchRequest())
 				);
+				Console.WriteLine("..Search Async....");
 
-				client.IndexDocument(new MyDocument { Id = "1" });
+				// sync
+				Agent.Tracer.CaptureTransaction("Sync Call", ApiConstants.TypeDb, () =>
+					client.Search<object>(new SearchRequest())
+				);
+				Console.WriteLine("..Search Sync....");
 
-				for (var i = 0; i < 10; i++)
-				{
-					// async
-					await Agent.Tracer.CaptureTransaction("Async Call", ApiConstants.TypeDb, async () =>
-						await client.SearchAsync<object>(new SearchRequest())
-					);
-					Console.WriteLine("..Search Async....");
-
-					// sync
-					Agent.Tracer.CaptureTransaction("Sync Call", ApiConstants.TypeDb, () =>
-						client.Search<object>(new SearchRequest())
-					);
-					Console.WriteLine("..Search Sync....");
-
-					// send a request that fails server validation
-					var r = await Agent.Tracer.CaptureTransaction("Bad Request", ApiConstants.TypeDb, async () =>
-						await client.SearchAsync<object>(new SearchRequest()
-						{
-							Size = int.MaxValue
-						})
-					);
-					Console.WriteLine($"..Bad Request.... ({r.ApiCall.HttpStatusCode})");
-				}
+				// send a request that fails server validation
+				var r = await Agent.Tracer.CaptureTransaction("Bad Request", ApiConstants.TypeDb, async () =>
+					await client.SearchAsync<object>(new SearchRequest("unknown-index")
+					{
+						//Size = int.MaxValue
+					})
+				);
+				Console.WriteLine($"..Bad Request.... ({r.ApiCall.HttpStatusCode})");
 			}
 		}
+
+		/// <summary>
+		/// Create a client that connects to cloud, uses dotnet user-secrets.
+		/// </summary>
+		private static ElasticClient CreateCloudClient()
+		{
+			var userSecrets = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+			var basicAuth = new BasicAuthenticationCredentials(userSecrets["cloud:user"], userSecrets["cloud:password"]);
+			var connectionPool = new CloudConnectionPool(userSecrets["cloud:id"], basicAuth);
+			var settings = DefaultConnectionSettings(new ConnectionSettings(connectionPool));
+			return new ElasticClient(settings);
+		}
+
+		/// <summary>
+		/// Create a client that connects against a cluster started with: https://github.com/elastic/apm-integration-testing
+		/// </summary>
+		private static ElasticClient CreateLocalClient()
+		{
+			var connectionPool = new StaticConnectionPool(new[] { new Uri("http://localhost:9200") });
+			var settings = DefaultConnectionSettings(new ConnectionSettings(connectionPool))
+				.BasicAuthentication("admin", "changeme");
+			return new ElasticClient(settings);
+		}
 	}
-}
+};
