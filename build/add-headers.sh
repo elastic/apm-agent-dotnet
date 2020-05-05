deedae76ec466f467ee0b1501fee2439c5e79b88
Martijn Laarman <Mpdreamz@gmail.com>
2020-05-05 10:54:37 +0200

    Add-headers script can now deal with BOM
    and will reinsert the marker post concatenation

--- build/add-headers.sh
+++ build/add-headers.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
+set -e pipefail
 
 script_path=$(dirname $(realpath -s $0))/../
 
-
 (find "$script_path" -name "*.fs" | grep -v "/bin/" | grep -v "/obj/")|while read fname; do
-    awk '{ if (NR==1) sub(/^\xef\xbb\xbf/,""); print }' $fname > $fname
+    bomb=$(hexdump -n 3 -C  "$fname" | head -n 1 | sed -e 's/[[:space:]]*//g' | cut -c 8-14 )
     line=$(head -n 1 "$fname")
     if [[ "$line" =~ ^/ ]] || [[ "$line" =~ ^# ]] ; then 
       echo "Skipped already starts with / or #: $fname"
     else 
-      cat "${script_path}build/file-header.txt" "$fname" > "${fname}.new"
+      if [[ "$bomb" == "0efbbbf" ]]; then
+        printf '\xEF\xBB\xBF' > "${fname}.new"
+      fi
+      sed -i $'1s/^\uFEFF//' $fname
+      cat "${script_path}build/file-header.txt" "$fname" >> "${fname}.new"
       mv "${fname}.new" "$fname"
     fi
 done
 
 (find "$script_path" -name "*.cs" | grep -v "/bin/" | grep -v "/obj/")|while read fname; do
-    awk '{ if (NR==1) sub(/^\xef\xbb\xbf/,""); print }' $fname > $fname
+    bomb=$(hexdump -n 3 -C  "$fname" | head -n 1 | sed -e 's/[[:space:]]*//g' | cut -c 8-14 )
     line=$(head -n 1 "$fname")
     if [[ "$line" =~ ^/ ]] || [[ "$line" =~ ^# ]] ; then 
       echo "Skipped already starts with / or #: $fname"
     else 
-      cat "${script_path}build/file-header.txt" "$fname" > "${fname}.new"
+      if [[ "$bomb" == "0efbbbf" ]]; then
+        printf '\xEF\xBB\xBF' > "${fname}.new"
+      fi
+      sed -i $'1s/^\uFEFF//' $fname
+      cat "${script_path}build/file-header.txt" "$fname" >> "${fname}.new"
       mv "${fname}.new" "$fname"
     fi
 done
