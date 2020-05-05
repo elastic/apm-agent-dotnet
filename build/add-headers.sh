f71c401e6f0a47b9eb3c7665f0bd0b0463be225c
Martijn Laarman <Mpdreamz@gmail.com>
2020-05-05 11:23:00 +0200

    refactor add-headers so that it can be run idempotently

--- build/add-headers.sh
+++ build/add-headers.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 set -e pipefail
 
+# anchor to the root
 script_path=$(dirname $(realpath -s $0))/../
 
-(find "$script_path" -name "*.fs" | grep -v "/bin/" | grep -v "/obj/")|while read fname; do
-    bomb=$(hexdump -n 3 -C  "$fname" | head -n 1 | sed -e 's/[[:space:]]*//g' | cut -c 8-14 )
-    line=$(head -n 1 "$fname")
-    if [[ "$line" =~ ^/ ]] || [[ "$line" =~ ^# ]] ; then 
-      echo "Skipped already starts with / or #: $fname"
-    else 
-      if [[ "$bomb" == "0efbbbf" ]]; then
-        printf '\xEF\xBB\xBF' > "${fname}.new"
-      fi
-      sed -i $'1s/^\uFEFF//' $fname
-      cat "${script_path}build/file-header.txt" "$fname" >> "${fname}.new"
-      mv "${fname}.new" "$fname"
-    fi
-done
+bomb_chars='\xEF\xBB\xBF'
+file_header_present_re='^(/|#)'
 
-(find "$script_path" -name "*.cs" | grep -v "/bin/" | grep -v "/obj/")|while read fname; do
-    bomb=$(hexdump -n 3 -C  "$fname" | head -n 1 | sed -e 's/[[:space:]]*//g' | cut -c 8-14 )
-    line=$(head -n 1 "$fname")
-    if [[ "$line" =~ ^/ ]] || [[ "$line" =~ ^# ]] ; then 
-      echo "Skipped already starts with / or #: $fname"
-    else 
-      if [[ "$bomb" == "0efbbbf" ]]; then
-        printf '\xEF\xBB\xBF' > "${fname}.new"
+addLicenseHeader() {
+  (find "$script_path" -name $1 | grep -v "/bin/" | grep -v "/obj/")|while read fname; do
+      bomb=$(hexdump -n 3 -C  "$fname" | head -n 1 | sed -e 's/[[:space:]]*//g' | cut -c 8-14 )
+      line=$(head -n 1 "$fname" | sed -e "s/^\xef\xbb\xbf//")
+      if [[ "$line" =~ $file_header_present_re ]]; then 
+        echo "Skipped already starts with / or #: $fname"
+      else 
+        if [[ "$bomb" == "0efbbbf" ]]; then
+          printf $bomb_chars > "${fname}.new"
+        fi
+        sed -i $'1s/^\uFEFF//' $fname
+        cat "${script_path}build/file-header.txt" "$fname" >> "${fname}.new"
+        mv "${fname}.new" "$fname"
       fi
-      sed -i $'1s/^\uFEFF//' $fname
-      cat "${script_path}build/file-header.txt" "$fname" >> "${fname}.new"
-      mv "${fname}.new" "$fname"
-    fi
-done
+  done
+}
+
+addLicenseHeader "*.cs"
+addLicenseHeader "*.fs"
