2182be81af478390111061e3edee280c4cafa3b9
Martijn Laarman <Mpdreamz@gmail.com>
2020-05-05 10:55:19 +0200

    Add file headers everywhere

--- src/Elastic.Apm.NetCoreAll/ApmMiddlewareExtension.cs
+++ src/Elastic.Apm.NetCoreAll/ApmMiddlewareExtension.cs
@@ -1,4 +1,8 @@
-﻿using Elastic.Apm.DiagnosticSource;
+﻿// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using Elastic.Apm.DiagnosticSource;
 using Elastic.Apm.EntityFrameworkCore;
 using Elastic.Apm.SqlClient;
 using Microsoft.AspNetCore.Builder;
