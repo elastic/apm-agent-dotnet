886b134d54a05b857d63628ce9d34e899d63f1ff
Daniel Rusznyak <daniel@rusznyak.com>
2020-09-07 17:52:11 +0100

    Fix NotFoundRoute_ShouldBe_Aggregatable test

--- test/Elastic.Apm.AspNetCore.Tests/TransactionNameTests.cs
+++ test/Elastic.Apm.AspNetCore.Tests/TransactionNameTests.cs
@@ -127,7 +127,7 @@ namespace Elastic.Apm.AspNetCore.Tests
 		{
 			var httpClient = Helper.GetClient(_agent, _factory, diagnosticSourceOnly);
 			await httpClient.GetAsync("home/doesnotexist");
-			await httpClient.GetAsync("css/doesnotexist/somefile.css");
+			await httpClient.GetAsync("files/doesnotexist/somefile");
 
 			_payloadSender.Transactions.Should().OnlyContain(n => n.Name.Equals("GET unknown route", StringComparison.OrdinalIgnoreCase));
 			_payloadSender.Transactions.Should().HaveCount(2);
