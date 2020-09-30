34adfdb6ecfcf01e196211d9b6b2300821ed2e89
Greg Kalapos <gergo@kalapos.net>
2020-09-30 20:08:57 +0200

    Use http in gRPC test instead of https
    This makes things in CI easier.

--- test/Elastic.Apm.Grpc.Tests/GrpcTests.cs
+++ test/Elastic.Apm.Grpc.Tests/GrpcTests.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Elastic.Apm.Api;
 using Elastic.Apm.AspNetCore.DiagnosticListener;
 using Elastic.Apm.DiagnosticSource;
 using Elastic.Apm.GrpcClient;
+using Elastic.Apm.Model;
 using Elastic.Apm.Tests.Mocks;
 using FluentAssertions;
 using Grpc.Net.Client;
 using GrpcServiceSample;
 using Xunit;
+
+#pragma warning disable CS0436 // Type conflicts with imported type
 
 namespace Elastic.Apm.Grpc.Tests
 {
@@ -31,7 +35,11 @@ namespace Elastic.Apm.Grpc.Tests
 
 			await sampleAppHost.StartAsync();
 
-			var channel = GrpcChannel.ForAddress("https://localhost:5001");
+			// This switch must be set before creating the GrpcChannel/HttpClient.
+			AppContext.SetSwitch(
+				"System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+			var channel = GrpcChannel.ForAddress(SampleAppHostBuilder.SampleAppUrl);
 			var client = new Greeter.GreeterClient(channel);
 
 			await apmAgent.Tracer.CaptureTransaction("SampleCall", "test", async () =>
@@ -46,6 +54,16 @@ namespace Elastic.Apm.Grpc.Tests
 			payloadSender.Transactions.Should()
 				.Contain(transaction => transaction.Result == "OK"
 						&& transaction.Name == $"/{Greeter.Descriptor.FullName}/{nameof(client.SayHello)}");
+
+			var vv = (payloadSender.Spans[1]).Context.Destination;
+			payloadSender.Spans.Should().Contain(span => span.Subtype == ApiConstants.SubTypeGrpc
+					&& span.Name == $"/{Greeter.Descriptor.FullName}/{nameof(client.SayHello)}"
+					&& span.Context.Destination.Address == "localhost"
+					&& span.Context.Destination.Port == SampleAppHostBuilder.SampleAppPort
+					&& span.Context.Destination.Service.Type == "external"
+					&& span.Context.Destination.Service.Name == SampleAppHostBuilder.SampleAppUrl
+					&& span.Context.Destination.Service.Resource == $"localhost:{SampleAppHostBuilder.SampleAppPort}"
+					);
 
 			await sampleAppHost.StopAsync();
 		}
--- test/Elastic.Apm.Grpc.Tests/SampleAppHostBuilder.cs
+++ test/Elastic.Apm.Grpc.Tests/SampleAppHostBuilder.cs
@@ -6,12 +6,16 @@ namespace Elastic.Apm.Grpc.Tests
 {
 	public class SampleAppHostBuilder
 	{
+		public static int SampleAppPort = 5866;
+		public static string SampleAppUrl = $"http://localhost:{SampleAppPort}";
+		
 		public IHost BuildHost()
 		{
 			var iHost =
 			Host.CreateDefaultBuilder()
 			.ConfigureWebHostDefaults(webBuilder =>
 			{
+				webBuilder.UseUrls(SampleAppUrl);
 				webBuilder.UseStartup<Startup>();
 			}).Build();
 
