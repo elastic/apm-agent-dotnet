54cc7ff1ade809815ccd1a02381fb234d440154a
Russ Cam <russ.cam@forloop.co.uk>
2020-10-14 13:45:40 +1000

    additional tidy up

--- sample/AspNetFullFrameworkSampleApp/Models/ResetPasswordViewModel.cs
+++ sample/AspNetFullFrameworkSampleApp/Models/ResetPasswordViewModel.cs
@@ -15,7 +15,7 @@ namespace AspNetFullFrameworkSampleApp.Models
 		public string Email { get; set; }
 
 		[Required]
-		[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+		[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 1)]
 		[DataType(DataType.Password)]
 		[Display(Name = "Password")]
 		public string Password { get; set; }
