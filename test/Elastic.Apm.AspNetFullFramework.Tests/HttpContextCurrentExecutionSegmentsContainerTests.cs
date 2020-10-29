93dfd38595eefc79ddf5d80a44912329f69b902d
Russ Cam <russ.cam@forloop.co.uk>
2020-10-29 13:13:01 +1000

    Add concurrent requests test
    This commit adds an integration test for
    HttpContextCurrentExecutionSegmentsContainer
    that makes multiple concurrent requests
    and asserts that the captured transactions
    and span ids align with expectations.

--- test/Elastic.Apm.AspNetFullFramework.Tests/HttpContextCurrentExecutionSegmentsContainerTests.cs
+++ test/Elastic.Apm.AspNetFullFramework.Tests/HttpContextCurrentExecutionSegmentsContainerTests.cs
@@ -3,6 +3,9 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -83,6 +86,73 @@ namespace Elastic.Apm.AspNetFullFramework.Tests
 
 				received.Spans.Count.Should().Be(103);
 			});
+		}
+
+		[AspNetFullFrameworkFact]
+		public async Task Transaction_And_Spans_Captured_When_Multiple_Concurrent_Requests()
+		{
+			static HttpRequestMessage CreateMessage(int i)
+			{
+				var message = new HttpRequestMessage(HttpMethod.Get, Consts.SampleApp.CreateUrl("/Home/Contact"));
+				message.Headers.Add("X-HttpRequest", i.ToString(CultureInfo.InvariantCulture));
+				return message;
+			}
+
+			var count = 9;
+			var messages = Enumerable.Range(1, count)
+				.Select(i => CreateMessage(i))
+				.ToList();
+
+			// infinite timespan
+			var client = new HttpClient { Timeout = TimeSpan.FromMilliseconds(-1) };
+
+			var tasks = new List<Task<HttpResponseMessage>>(messages.Count);
+			foreach (var message in messages)
+				tasks.Add(client.SendAsync(message));
+
+			await Task.WhenAll(tasks).ConfigureAwait(false);
+
+			for (var index = 0; index < tasks.Count; index++)
+			{
+				var task = tasks[index];
+				task.IsCompletedSuccessfully.Should().BeTrue();
+				var response = task.Result;
+				var responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+				response.IsSuccessStatusCode.Should().BeTrue($"response {index}: {responseContent}");
+			}
+
+			await WaitAndCustomVerifyReceivedData(received =>
+			{
+				received.Transactions.Count.Should().Be(count * 2);
+
+				var contactTransactions = received.Transactions
+					.Where(t => t.Name == "GET Home/Contact")
+					.ToList();
+
+				contactTransactions.Should().HaveCount(count);
+
+				var aboutTransactions = received.Transactions
+					.Where(t => t.Name == "GET Home/About")
+					.ToList();
+
+				aboutTransactions.Should().HaveCount(count);
+
+				// assert that each aboutTransaction is a child of a span associated with a contactTransaction
+				foreach (var contactTransaction in contactTransactions)
+				{
+					contactTransaction.ParentId.Should().BeNull();
+					var spans = received.Spans.Where(s => s.TransactionId == contactTransaction.Id)
+						.ToList();
+
+					spans.Should().HaveCount(2);
+
+					var localHostSpan = spans.SingleOrDefault(s => s.Name == "GET localhost");
+					localHostSpan.Should().NotBeNull();
+
+					var aboutTransaction = aboutTransactions.SingleOrDefault(t => t.ParentId == localHostSpan.Id);
+					aboutTransaction.Should().NotBeNull();
+				}
+			}).ConfigureAwait(false);
 		}
 	}
 }
