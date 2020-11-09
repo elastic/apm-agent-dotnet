02e2701a6a8184c14b25ec94403f941a2b980da7
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-11-09 15:56:55 +0000

    Use the preview https://chocolatey.org/packages/visualstudio2019buildtools-preview/16.8.0.60000-preview1

--- .ci/windows/msbuild-tools.ps1
+++ .ci/windows/msbuild-tools.ps1
@@ -3,6 +3,6 @@
 #
 
 # Install visualstudio2019buildtools
-& choco install visualstudio2019buildtools -m -y --no-progress --force -r --version=16.7.7.0
-& choco install visualstudio2019professional -m -y --no-progress --force -r --version=16.7.7.0 --package-parameters "--includeRecommended --includeOptional"
+& choco install visualstudio2019buildtools-preview -m -y --no-progress --force -r --version=16.8.0.60000-preview1
+& choco install visualstudio2019professional-preview -m -y --no-progress --force -r --version=16.8.0.60000-preview1 --package-parameters "--includeRecommended --includeOptional"
 & choco install visualstudio2019-workload-netweb -m -y --no-progress --force -r --version=1.0.0
