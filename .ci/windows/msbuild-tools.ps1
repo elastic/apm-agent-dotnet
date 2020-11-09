131aa23f56f8e3db4f373fe9b6ae33a0cb277d01
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-11-09 16:36:32 +0000

    Fail if something bad happened when installing the tools

--- .ci/windows/msbuild-tools.ps1
+++ .ci/windows/msbuild-tools.ps1
@@ -4,5 +4,13 @@
 
 # Install visualstudio2019buildtools
 & choco install visualstudio2019buildtools-preview -m -y --no-progress --force -r --version=16.8.0.60000-preview1
+if ($? -ne 0) {
+  Write-Host "visualstudio2019buildtools-preview installation failed. See the logs."
+  exit 1
+}
 & choco install visualstudio2019professional-preview -m -y --no-progress --force -r --version=16.8.0.60000-preview1 --package-parameters "--includeRecommended --includeOptional"
+if ($? -ne 0) {
+  Write-Host "visualstudio2019professional-preview installation failed. See the logs."
+  exit 1
+}
 & choco install visualstudio2019-workload-netweb -m -y --no-progress --force -r --version=1.0.0
