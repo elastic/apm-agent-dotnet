32e146fec897c55332d3a9256e255e1113b4d21b
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-11-09 14:23:50 +0000

    Bump msbuild dependencies
    https://chocolatey.org/packages/visualstudio2019buildtools/16.7.7.0

--- .ci/windows/msbuild-tools.ps1
+++ .ci/windows/msbuild-tools.ps1
@@ -3,6 +3,6 @@
 #
 
 # Install visualstudio2019buildtools
-& choco install visualstudio2019buildtools -m -y --no-progress --force -r --version=16.4.5.0
-& choco install visualstudio2019professional -m -y --no-progress --force -r --version=16.4.5.0 --package-parameters "--includeRecommended --includeOptional"
+& choco install visualstudio2019buildtools -m -y --no-progress --force -r --version=16.7.7.0
+& choco install visualstudio2019professional -m -y --no-progress --force -r --version=16.7.7.0 --package-parameters "--includeRecommended --includeOptional"
 & choco install visualstudio2019-workload-netweb -m -y --no-progress --force -r --version=1.0.0
