dda65bea152a5284c0df2016f1fac4f0921b9193
Russ Cam <russ.cam@forloop.co.uk>
2020-11-16 17:16:50 +1000

    Fix error for RazorTagHelper - DOTNET_HOST_PATH is not set

--- .ci/windows/msbuild.bat
+++ .ci/windows/msbuild.bat
@@ -4,4 +4,9 @@
 echo "Prepare context for VsDevCmd.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat"
 nuget restore -verbosity detailed -NonInteractive
+
+rem TODO: Workaround for https://github.com/dotnet/sdk/issues/14497
+setx DOTNET_HOST_PATH "%ProgramFiles%\dotnet\dotnet.exe" /M
+rmdir /s /q "%ProgramFiles%\dotnet\sdk\NuGetFallbackFolder"
+
 msbuild
