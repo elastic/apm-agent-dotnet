3139bfcf1a912c6cbdbe74c6285e6b532dcb9188
Russ Cam <russ.cam@forloop.co.uk>
2020-11-16 14:41:42 +1000

    Use $LASTEXITCODE
    This commit uses $LASTEXITCODE instead of $?.
    The latter returns $true or $false for success
    whereas the exit code is needed for comparison.

--- .ci/windows/msbuild-tools.ps1
+++ .ci/windows/msbuild-tools.ps1
@@ -2,27 +2,19 @@
 # This script installs the required tools to be used during the msbuild
 #
 
-function Write-ChocolateyLogs() {
-    $chocoLogs = Get-Content -Raw -Path "C:\ProgramData\chocolatey\logs\chocolatey.log"
-    Write-Host $chocoLogs
-}
-
 # Install visualstudio2019buildtools
 choco install visualstudio2019buildtools -m -y --no-progress --force -r --version=16.8.1
-if ($? -ne 0) {
+if ($LASTEXITCODE -ne 0) {
   Write-Host "visualstudio2019buildtools installation failed."
-  Write-ChocolateyLogs
   exit 1
 }
 choco install visualstudio2019professional -m -y --no-progress --force -r --version=16.8.1 --package-parameters "--includeRecommended --includeOptional"
-if ($? -ne 0) {
+if ($LASTEXITCODE -ne 0) {
   Write-Host "visualstudio2019professional installation failed."
-  Write-ChocolateyLogs
   exit 1
 }
 choco install visualstudio2019-workload-netweb -m -y --no-progress --force -r --version=1.0.0
-if ($? -ne 0) {
+if ($LASTEXITCODE -ne 0) {
   Write-Host "visualstudio2019-workload-netweb installation failed."
-  Write-ChocolateyLogs
   exit 1
 }
