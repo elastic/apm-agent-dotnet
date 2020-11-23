4e22ddb71a5e82f8793e4c07d5baa32852113f56
Greg Kalapos <gergo@kalapos.net>
2020-11-23 20:54:30 +0100

    HostBuilderExtensions.UseElasticApm: subscribe to `subscribers` parameter
    Fixes #1059

--- test/Elastic.Apm.Extensions.Hosting.Tests/HostBuilderExtensionTests.cs
+++ test/Elastic.Apm.Extensions.Hosting.Tests/HostBuilderExtensionTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Elastic.Apm.DiagnosticSource;
+using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SampleConsoleNetCoreApp;
@@ -24,9 +27,49 @@ namespace Elastic.Apm.Extensions.Hosting.Tests
 			await Task.WhenAll(hostBuilder1.StopAsync(), hostBuilder2.StopAsync());
 		}
 
+		/// <summary>
+		/// Makes sure that <see cref="Agent.IsConfigured" /> is <code>true</code> after the agent is enabled through
+		/// <see cref="HostBuilderExtensions.UseElasticApm" />.
+		/// </summary>
+		[Fact]
+		public void IsAgentInitializedAfterUseElasticApm()
+		{
+			var _ = CreateHostBuilder().Build();
+			Agent.IsConfigured.Should().BeTrue();
+		}
+
+		/// <summary>
+		/// Makes sure that agent enables the <see cref="IDiagnosticsSubscriber" /> passed into
+		/// <see cref="HostBuilderExtensions.UseElasticApm" />.
+		/// </summary>
+		[Fact]
+		public void DiagnosticSubscriberWithUseElasticApm()
+		{
+			var fakeSubscriber = new FakeSubscriber();
+			fakeSubscriber.IsSubscribed.Should().BeFalse();
+
+			Host.CreateDefaultBuilder()
+				.ConfigureServices((context, services) => { services.AddHostedService<HostedService>(); })
+				.UseElasticApm(fakeSubscriber)
+				.Build();
+
+			fakeSubscriber.IsSubscribed.Should().BeTrue();
+		}
+
 		private static IHostBuilder CreateHostBuilder() =>
-				Host.CreateDefaultBuilder()
-					.ConfigureServices((context, services) => { services.AddHostedService<HostedService>(); })
-					.UseElasticApm();
+			Host.CreateDefaultBuilder()
+				.ConfigureServices((context, services) => { services.AddHostedService<HostedService>(); })
+				.UseElasticApm();
+
+		public class FakeSubscriber : IDiagnosticsSubscriber
+		{
+			public bool IsSubscribed { get; set; }
+
+			public IDisposable Subscribe(IApmAgent components)
+			{
+				IsSubscribed = true;
+				return null;
+			}
+		}
 	}
 }
