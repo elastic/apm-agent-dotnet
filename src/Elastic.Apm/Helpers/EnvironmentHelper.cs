b43df677e702275758110a80c34bf7d133b3edae
Russ Cam <russ.cam@elastic.co>
2020-12-16 14:09:12 +1000

    [META 388] Add support for Azure App Service Cloud metadata (#1083)
    This commit adds support for retrieving
    cloud metadata about an Azure App Service
    from the environment variables.

---
+++ src/Elastic.Apm/Helpers/EnvironmentHelper.cs
@@ -0,0 +1,31 @@
+// Licensed to Elasticsearch B.V under
+// one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System;
+using System.Collections;
+using Elastic.Apm.Logging;
+
+namespace Elastic.Apm.Helpers
+{
+	/// <summary>
+	/// Gets Environment variables, catching and logging any exception that may be thrown.
+	/// </summary>
+	internal static class EnvironmentHelper
+	{
+		public static IDictionary GetEnvironmentVariables(IApmLogger logger)
+		{
+			try
+			{
+				return Environment.GetEnvironmentVariables();
+			}
+			catch (Exception e)
+			{
+				logger.Debug()?.LogException(e, "Error while getting environment variables");
+			}
+
+			return null;
+		}
+	}
+}
