67e82f63f364d2adf6361c9dc0be6514fde2c99d
Russ Cam <russ.cam@forloop.co.uk>
2020-12-23 12:28:42 +1000

    Add Microsoft.NET.Sdk.Razor package reference for ASP.NET Core 2.2
    This commit adds a reference to Microsoft.NET.Sdk.Razor
    package reference for ASP.NET Core 2.2. This is to fix a
    known bug in the .NET 5.0 SDK where the build target
    RazorTagHelper fails when DOTNET_HOST_PATH is not set.

--- .ci/windows/msbuild.bat
+++ .ci/windows/msbuild.bat
@@ -5,7 +5,4 @@ echo "Prepare context for VsDevCmd.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat"
 nuget restore -verbosity detailed -NonInteractive
 
-rem TODO: Workaround for https://github.com/dotnet/sdk/issues/14497
-set DOTNET_HOST_PATH=dotnet
-
 msbuild
