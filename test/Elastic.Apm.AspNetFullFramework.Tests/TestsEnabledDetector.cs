3fb4b125cee61d0f32cbd641adf25667e1b26726
Russ Cam <russ.cam@forloop.co.uk>
2020-12-30 17:03:19 +1000

    Remove unneeded using directives

--- test/Elastic.Apm.AspNetFullFramework.Tests/TestsEnabledDetector.cs
+++ test/Elastic.Apm.AspNetFullFramework.Tests/TestsEnabledDetector.cs
@@ -6,7 +6,6 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
-using Elastic.Apm.Tests.TestHelpers;
 using Elastic.Apm.Tests.Utilities;
 
 namespace Elastic.Apm.AspNetFullFramework.Tests
