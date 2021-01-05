2f114adc8b3d8accd4717bf2b270d7a603dbae3f
Russ Cam <russ.cam@forloop.co.uk>
2021-01-06 09:34:33 +1000

    Reinstate code coverage on linux tests

--- .ci/linux/test.sh
+++ .ci/linux/test.sh
@@ -14,14 +14,14 @@ dotnet test ElasticApmAgent.sln \
 	--results-directory target \
 	--diag "target/diag-ElasticApmAgent.log" \
 	--logger:"junit;LogFilePath=junit-{framework}-{assembly}.xml;MethodFormat=Class;FailureBodyFormat=Verbose" \
-#	--collect:"XPlat Code Coverage" \
-#	--settings coverlet.runsettings \
-#	/p:CollectCoverage=true \
-#	/p:CoverletOutputFormat=cobertura \
-#	/p:CoverletOutput=target/Coverage/ \
-#	/p:Threshold=0 \
-#	/p:ThresholdType=branch \
-#	/p:ThresholdStat=total \
+	--collect:"XPlat Code Coverage" \
+	--settings coverlet.runsettings \
+	/p:CollectCoverage=true \
+	/p:CoverletOutputFormat=cobertura \
+	/p:CoverletOutput=target/Coverage/ \
+	/p:Threshold=0 \
+	/p:ThresholdType=branch \
+	/p:ThresholdStat=total \
 	|| echo -e "\033[31;49mTests FAILED\033[0m"
 
 echo 'Move coverage files if they were generated!'
