a3125c771c33036a3f538a9f8febf3239d72528d
Russ Cam <russ.cam@forloop.co.uk>
2021-01-07 16:20:23 +1000

    Disable nodereuse on linux tests
    trying https://github.com/dotnet/sdk/issues/9452#issuecomment-417681156

--- .ci/linux/test.sh
+++ .ci/linux/test.sh
@@ -10,6 +10,7 @@ set -euxo pipefail
 
 # Run tests for all solution
 dotnet test ElasticApmAgent.sln \
+	-nodereuse:false \
 	--verbosity normal \
 	--results-directory target \
 	--diag "target/diag-ElasticApmAgent.log" \
