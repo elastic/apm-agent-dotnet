d3281875660a28777a21dd477ea4162657ee9ab1
Greg Kalapos <gergo@kalapos.net>
2021-01-15 20:52:00 +0100

    Set _isConfigured flag also with implicit agent initialization

---
+++ test/Elastic.Apm.Tests/StaticImplicitInitializationTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Elastic.Apm.Tests
+{
+	public class StaticImplicitInitializationTests
+	{
+		/// <summary>
+		/// Makes sure Agent.IsConfigured is true after implicit agent initialization
+		/// </summary>
+		[Fact]
+		public void IsConfiguredWithImplicitInitialization()
+		{
+			Agent.IsConfigured.Should().BeFalse();
+
+			Agent.Tracer.CaptureTransaction("Foo", "Bar", () => { });
+			Agent.IsConfigured.Should().BeTrue();
+		}
+	}
+}
