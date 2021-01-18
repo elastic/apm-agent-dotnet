c00a2e7991b12e3ff0e559110c2aa9d2d467f33f
Russ Cam <russ.cam@forloop.co.uk>
2021-01-18 16:33:08 +1000

    Use coverlet settings and remove --no-build

--- .ci/windows/test.bat
+++ .ci/windows/test.bat
@@ -2,16 +2,15 @@
 :: This script runs the tests and stored them in an xml file defined in the
 :: LogFilePath property
 ::
-dotnet test -c Release --no-build ^
+dotnet test -c Release ^
  --verbosity normal ^
  --results-directory target ^
  --diag target\diag.log ^
  --logger:"junit;LogFilePath=junit-{framework}-{assembly}.xml;MethodFormat=Class;FailureBodyFormat=Verbose" ^
+ --settings coverlet.runsettings ^
  /p:CollectCoverage=true ^
  /p:CoverletOutputFormat=cobertura ^
  /p:CoverletOutput=target\Coverage\ ^
- /p:Include=\"[Elastic.*]*,[ElasticApmStartupHook]*\" ^
- /p:Exclude=\"[Elastic.Apm.Tests]*,[Elastic.Apm.*.Tests]*,[Elastic.Apm.Tests.*]*,[Elastic.Apm.Benchmarks]*\" ^
  /p:Threshold=0 ^
  /p:ThresholdType=branch ^
  /p:ThresholdStat=total
