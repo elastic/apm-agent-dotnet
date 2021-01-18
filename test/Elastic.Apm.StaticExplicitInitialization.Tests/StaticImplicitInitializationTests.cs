f89b43eb30d53197ca004dc54c3ce2feb39ac140
Gergely Kalapos <gergo@kalapos.net>
2021-01-18 19:56:10 +0100

    Set _isConfigured flag also with implicit agent initialization (#1137)
    * Set _isConfigured flag also with implicit agent initialization
    
    * Move static tests into separate folders
    
    * Remove net461 from static tests

---
+++ test/Elastic.Apm.StaticExplicitInitialization.Tests/StaticImplicitInitializationTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Elastic.Apm.StaticExplicitInitialization.Tests
+{
+	public class StaticImplicitInitializationTests
+	{
+		/// <summary>
+		/// Makes sure Agent.IsConfigured is true after implicit agent initialization
+		/// </summary>
+		[Fact]
+		public void IsConfiguredWithImplicitInitialization()
+		{
+			Agent.IsConfigured.Should().BeFalse();
+
+			Agent.Tracer.CaptureTransaction("Foo", "Bar", () => { });
+			Agent.IsConfigured.Should().BeTrue();
+		}
+	}
+}
