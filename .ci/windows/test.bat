d812bad7b2b4d629035bf0998b4401da9a8acca9
Russ Cam <russ.cam@forloop.co.uk>
2021-01-18 18:29:54 +1000

    don't build for tests

--- .ci/windows/test.bat
+++ .ci/windows/test.bat
@@ -2,7 +2,7 @@
 :: This script runs the tests and stored them in an xml file defined in the
 :: LogFilePath property
 ::
-dotnet test -c Release ^
+dotnet test -c Release --no-build ^
  --verbosity normal ^
  --results-directory target ^
  --diag target\diag.log ^
