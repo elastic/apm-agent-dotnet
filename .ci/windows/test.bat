ad928f1228d27205e3ea1aa3be5aba490fd44281
Russ Cam <russ.cam@forloop.co.uk>
2021-01-29 13:12:16 +1000

    Use coverlet.collector for code coverage

--- .ci/windows/test.bat
+++ .ci/windows/test.bat
@@ -6,4 +6,12 @@ dotnet test -c Release --no-build ^
  --verbosity normal ^
  --results-directory target ^
  --diag target\diag.log ^
- --logger:"junit;LogFilePath=junit-{framework}-{assembly}.xml;MethodFormat=Class;FailureBodyFormat=Verbose"
+ --logger:"junit;LogFilePath=junit-{framework}-{assembly}.xml;MethodFormat=Class;FailureBodyFormat=Verbose" ^
+ --collect:"XPlat Code Coverage" ^
+ --settings coverlet.runsettings ^
+ /p:CollectCoverage=true ^
+ /p:CoverletOutputFormat=cobertura ^
+ /p:CoverletOutput=target/Coverage/ ^
+ /p:Threshold=0 ^
+ /p:ThresholdType=branch ^
+ /p:ThresholdStat=total
