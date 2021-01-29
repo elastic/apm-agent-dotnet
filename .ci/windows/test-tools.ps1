97778790a1f244fb0f01b9c3948567a22f012fe6
Russ Cam <russ.cam@forloop.co.uk>
2021-01-29 13:11:58 +1000

    Remove addition of JunitXml.TestLogger
    added in Directory.Build.props in test dir

--- .ci/windows/test-tools.ps1
+++ .ci/windows/test-tools.ps1
@@ -4,9 +4,3 @@
 
 # Install tools
 dotnet tool install -g Codecov.Tool --version 1.2.0
-
-# Add coverlet.msbuild only to test projects
-Get-ChildItem -Path ./test -Recurse -Filter *.csproj |
-Foreach-Object {
-  dotnet add $_.FullName package JunitXml.TestLogger --version 2.1.78
-}
