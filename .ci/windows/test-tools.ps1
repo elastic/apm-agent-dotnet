1848e22c4acabc190c7178c7cbac0e6458641e65
Russ Cam <russ.cam@forloop.co.uk>
2021-02-04 13:16:03 +1000

    don't break on codecov.tool already installed

--- .ci/windows/test-tools.ps1
+++ .ci/windows/test-tools.ps1
@@ -2,5 +2,7 @@
 # This script installs the required test tools to be used during the tests execution
 #
 
-# Install tools
-dotnet tool install -g Codecov.Tool --version 1.2.0
+# Install Codecov.tool. Redirect stderr to stdout so that already installed codecov.tool does not cause
+# Jenkins to abort.
+dotnet tool install -g Codecov.Tool --version 1.2.0 2>&1
+
