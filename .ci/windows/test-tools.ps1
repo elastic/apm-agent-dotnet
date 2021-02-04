3be2e91bfc5ae58903562f19f5f14cc9eed7c9e3
Russ Cam <russ.cam@forloop.co.uk>
2021-02-04 19:52:45 +1000

    check last exit code

--- .ci/windows/test-tools.ps1
+++ .ci/windows/test-tools.ps1
@@ -2,7 +2,9 @@
 # This script installs the required test tools to be used during the tests execution
 #
 
-# Install Codecov.tool. Redirect stderr to stdout so that already installed codecov.tool does not cause
-# Jenkins to abort.
-dotnet tool install -g Codecov.Tool --version 1.2.0 2>&1
+dotnet tool install -g Codecov.Tool --version 1.2.0
+if ($LASTEXITCODE) {
+    Write-Host "codecov.tool installation failed."
+    exit 1
+}
 
