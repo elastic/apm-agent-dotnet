c7b5a24c41caf7ebb77183faffc2cc1186892a93
Russ Cam <russ.cam@forloop.co.uk>
2021-02-10 13:32:52 +1000

    Remove coverlet

--- .ci/windows/test.bat
+++ .ci/windows/test.bat
@@ -6,11 +6,4 @@ dotnet test -c Release --no-build ^
  --verbosity normal ^
  --results-directory target ^
  --diag target\diag.log ^
- --logger:"junit;LogFilePath=junit-{framework}-{assembly}.xml;MethodFormat=Class;FailureBodyFormat=Verbose" ^
- /p:CollectCoverage=true ^
- /p:CoverletOutputFormat=cobertura ^
- /p:CoverletOutput=target\Coverage\ ^
- /p:Exclude=\"[Elastic.Apm.Tests]*,[SampleAspNetCoreApp*]*,[xunit*]*\" ^
- /p:Threshold=0 ^
- /p:ThresholdType=branch ^
- /p:ThresholdStat=total
+ --logger:"junit;LogFilePath=junit-{framework}-{assembly}.xml;MethodFormat=Class;FailureBodyFormat=Verbose"
