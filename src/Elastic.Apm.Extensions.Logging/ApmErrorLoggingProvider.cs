2002808a766d4693eaa0b6b55f0ca1331b831f08
Russ Cam <russ.cam@elastic.co>
2021-02-25 10:45:29 +1000

    Move InternalsVisibleTo into project files (#1201)
    This commit moves the usage of the InternalsVisibleTo into project files.
    The exception is AspNetFullFrameworkSampleApp, which uses the
    older project file format.
    
    (cherry picked from commit 608c7e532029e9461ab21b8ca419a110faff2be2)

--- src/Elastic.Apm.Extensions.Logging/ApmErrorLoggingProvider.cs
+++ src/Elastic.Apm.Extensions.Logging/ApmErrorLoggingProvider.cs
@@ -3,12 +3,7 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
-using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Logging;
-
-[assembly:
-	InternalsVisibleTo(
-		"Elastic.Apm.Extensions.Hosting, PublicKey=" + Signing.PublicKey)]
 
 namespace Elastic.Apm.Extensions.Logging
 {
