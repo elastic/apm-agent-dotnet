01deb0f951fad68f18e4627a12f1cd1efe26cd66
Russ Cam <russ.cam@elastic.co>
2021-03-09 11:38:11 +1000

    Get transaction name from Web API controller route template (#1189)
    This commit updates ElasticApmModule to get the transaction
    name for a Web API controller routed with attribute routing
    from the "MS_SubRoutes" route data value.
    
    A delegate to get the template from IHttpRouteData when
    System.Web.Http is referenced.
    
    Closes #1176

--- .ci/windows/msbuild-tools.ps1
+++ .ci/windows/msbuild-tools.ps1
@@ -3,12 +3,12 @@
 #
 
 # Install visualstudio2019buildtools
-choco install visualstudio2019buildtools -m -y --no-progress --force -r --version=16.8.5.0
+choco install visualstudio2019buildtools -m -y --no-progress --force -r --version=16.9.0.0
 if ($LASTEXITCODE -ne 0) {
   Write-Host "visualstudio2019buildtools installation failed."
   exit 1
 }
-choco install visualstudio2019professional -m -y --no-progress --force -r --version=16.8.5.0 --package-parameters "--includeRecommended --includeOptional"
+choco install visualstudio2019professional -m -y --no-progress --force -r --version=16.9.0.0 --package-parameters "--includeRecommended --includeOptional"
 if ($LASTEXITCODE -ne 0) {
   Write-Host "visualstudio2019professional installation failed."
   exit 1
