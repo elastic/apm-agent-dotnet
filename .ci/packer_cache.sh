648595b7ca60cea1493898aed485d88d79e4348b
Victor Martinez <victormartinezrubio@gmail.com>
2021-03-15 14:32:49 +0000

    Skip packer cache on ARM (#1224)

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -2,4 +2,10 @@
 # shellcheck disable=SC1091
 source /usr/local/bin/bash_standard_lib.sh
 
-docker build --tag docker.elastic.co/observability-ci/apm-agent-dotnet-sdk-linux:latest .ci/docker/sdk-linux
+ARCH=$(uname -m| tr '[:upper:]' '[:lower:]')
+
+if [ "${ARCH}" == "x86_64" ] ; then
+    docker build --tag docker.elastic.co/observability-ci/apm-agent-dotnet-sdk-linux:latest .ci/docker/sdk-linux
+else
+    echo "The existing docker image on ARM is not supported yet."
+fi
