93cb8b6242bc5eb5d3f9684a9b4680aa3a28c7b9
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2021-04-29 10:49:55 +0200

    test: permissions

--- .ci/windows/test-iis.bat
+++ .ci/windows/test-iis.bat
@@ -2,6 +2,7 @@ set ELASTIC_APM_TESTS_FULL_FRAMEWORK_ENABLED=true
 set sample_app_log_dir=C:\Elastic_APM_TEMP
 if not exist "%sample_app_log_dir%" mkdir "%sample_app_log_dir%"
 icacls %sample_app_log_dir% /t /grant Everyone:F
+icacls test\Elastic.Apm.AspNetFullFramework.Tests /t /grant Everyone:F
 set ELASTIC_APM_ASP_NET_FULL_FRAMEWORK_SAMPLE_APP_LOG_FILE=%sample_app_log_dir%\Elastic.Apm.AspNetFullFramework.Tests.SampleApp.log
 
 dotnet test -c Release test\Elastic.Apm.AspNetFullFramework.Tests --no-build ^
