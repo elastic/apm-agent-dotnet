bcb1d5920e56accac19168ecafd3fb1d5598989c
Russ Cam <russ.cam@forloop.co.uk>
2021-06-10 19:04:34 +1000

    Install VS 2019 Build tools
    This commit installs the Visual Studio 2019 Build tools with
    the needed workloads to support .NET Core projects whilst
    building and running tests for ASP.NET Full Framework.
    
    Use the installer as opposed to chocolatey packages, which
    is more reliable. The workloads are split into separate
    chocolatey packages, which is less desirable than installing
    all workloads on initial installation.
    
    Visual Studio 2019 Professional is not required.
    
    Invoke the VS 2019 Build tools Developer prompt.

--- .ci/windows/msbuild.bat
+++ .ci/windows/msbuild.bat
@@ -2,7 +2,7 @@
 :: This script runs the msbuild
 ::
 echo "Prepare context for VsDevCmd.bat"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat"
 nuget restore -verbosity detailed -NonInteractive
 
 msbuild /p:Configuration=Release
