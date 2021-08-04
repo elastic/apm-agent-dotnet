fd8ac0189c20608f3f2a5471d6eea2f1153409ef
Russ Cam <russ.cam@elastic.co>
2021-08-04 11:16:49 +1000

    Propagate Trace context in exit spans (#1358)
    This commit propagates the Trace Context for
    exit span outgoing requests, without constructing
    a child HTTP span, as per current behaviour.
    
    The Trace context propagated is retrieved from
    the current span.
    
    Closes #1350

---
+++ test/Elastic.Apm.Elasticsearch.Tests/TraceContextTests.cs
@@ -0,0 +1,45 @@
+// Licensed to Elasticsearch B.V under
+// one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System;
+using Elastic.Apm.Api;
+using Elastic.Apm.DiagnosticSource;
+using Elastic.Apm.Tests.Utilities;
+using Elasticsearch.Net;
+using FluentAssertions;
+using Xunit;
+
+namespace Elastic.Apm.Elasticsearch.Tests
+{
+	public class TraceContextTests
+	{
+		[Fact]
+		public void Call_to_Elasticsearch_propagates_Trace_Context_when_HttpDiagnosticsSubscriber_subscribed()
+		{
+			using var localServer = LocalServer.Create(context =>
+			{
+				var traceparent = context.Request.Headers.Get("traceparent");
+				traceparent.Should().NotBeNullOrEmpty();
+
+				var elasticTraceparent = context.Request.Headers.Get("elastic-apm-traceparent");
+				elasticTraceparent.Should().NotBeNullOrEmpty().And.Be(traceparent);
+
+				var tracestate = context.Request.Headers.Get("tracestate");
+				tracestate.Should().NotBeNullOrEmpty().And.Contain("es=s:1");
+
+				context.Response.StatusCode = 200;
+			});
+
+			using var agent = new ApmAgent(new TestAgentComponents(payloadSender: new MockPayloadSender()));
+			using var subscribe = agent.Subscribe(new ElasticsearchDiagnosticsSubscriber(), new HttpDiagnosticsSubscriber());
+
+			var client = new ElasticLowLevelClient(new ConnectionConfiguration(new Uri(localServer.Uri)));
+			agent.Tracer.CaptureTransaction("Transaction", ApiConstants.TypeDb, t =>
+			{
+				var response = client.Cat.Indices<StringResponse>();
+			});
+		}
+	}
+}
