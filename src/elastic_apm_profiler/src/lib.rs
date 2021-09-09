2cc5ac9adfd2280064d23950ac46827ca5d6ebfa
Russ Cam <russ.cam@forloop.co.uk>
2021-09-09 14:26:54 +1000

    Replace macro with simple exported function

--- src/elastic_apm_profiler/src/lib.rs
+++ src/elastic_apm_profiler/src/lib.rs
@@ -33,29 +33,19 @@ const IID_PROFILER: IID = IID {
     data4: [0x9B, 0x20, 0x95, 0xE0, 0x4F, 0x6C, 0x03, 0xCC],
 };
 
-macro_rules! dll_get_class_object {
-    (($class_id_one:ident, $class_type_one:ty), $(($class_id:ident, $class_type:ty)),*) => {
-        #[no_mangle]
-        unsafe extern "system" fn DllGetClassObject(
-            class_id: *const ::com::sys::CLSID,
-            iid: *const ::com::sys::IID,
-            result: *mut *mut ::core::ffi::c_void) -> ::com::sys::HRESULT {
+/// Called by the runtime to get an instance of the profiler
+#[no_mangle]
+unsafe extern "system" fn DllGetClassObject(
+    class_id: *const ::com::sys::CLSID,
+    iid: *const ::com::sys::IID,
+    result: *mut *mut ::core::ffi::c_void) -> ::com::sys::HRESULT {
 
-            assert!(!class_id.is_null(), "class id passed to DllGetClassObject should never be null");
-            let class_id = &*class_id;
-            if class_id == &$class_id_one {
-                let instance = <$class_type_one as ::com::production::Class>::Factory::allocate();
-                instance.QueryInterface(&*iid, result)
-            } $(else if class_id == &$class_id {
-                let instance = <$class_type_one as ::com::production::Class>::Factory::allocate();
-                instance.QueryInterface(&*iid, result)
-            })* else {
-                ::com::sys::CLASS_E_CLASSNOTAVAILABLE
-            }
-        }
-    };
+    assert!(!class_id.is_null(), "class id passed to DllGetClassObject should never be null");
+    let class_id = &*class_id;
+    if class_id == &IID_PROFILER {
+        let instance = <Profiler as ::com::production::Class>::Factory::allocate();
+        instance.QueryInterface(&*iid, result)
+    } else {
+        ::com::sys::CLASS_E_CLASSNOTAVAILABLE
+    }
 }
-
-// associates Profiler with a clsid so that an instance
-// can be created when the runtime asks for an instance by id when it calls DllGetClassObject
-dll_get_class_object![(IID_PROFILER, Profiler),];
