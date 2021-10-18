8a449978c8b39bad29e95d76007b490d7b5f3d13
Greg Kalapos <gergo@kalapos.net>
2021-10-18 23:57:43 +0200

    Update NetCoreAndNet5Fact.cs

--- test/Elastic.Apm.Tests/NetCoreAndNet5Fact.cs
+++ test/Elastic.Apm.Tests/NetCoreAndNet5Fact.cs
@@ -14,7 +14,7 @@ namespace Elastic.Apm.Tests
 		{
 			if (!(RuntimeInformation.FrameworkDescription.StartsWith(PlatformDetection.DotNetCoreDescriptionPrefix)
 				|| (RuntimeInformation.FrameworkDescription.StartsWith(PlatformDetection.DotNetPrefix) &&
-					RuntimeInformation.FrameworkDescription.StartsWith(PlatformDetection.DotNetFullFrameworkDescriptionPrefix))))
+					!RuntimeInformation.FrameworkDescription.StartsWith(PlatformDetection.DotNetFullFrameworkDescriptionPrefix))))
 			{
 				Skip =
 					$"{nameof(NetCoreAndNet5Fact)} tests only run on .NET Core and .NET 5 - test was executed on {RuntimeInformation.FrameworkDescription} - therefore test will be skipped";
