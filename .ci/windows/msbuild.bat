39718fe2e68d6ee0ad32ef1addf447e921c2f0a0
Russ Cam <russ.cam@forloop.co.uk>
2021-10-26 16:32:43 +1000

    Try adding nuget source

--- .ci/windows/msbuild.bat
+++ .ci/windows/msbuild.bat
@@ -4,6 +4,7 @@
 echo "Prepare context for VsDevCmd.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat"
 
+dotnet nuget add source --name nuget.org https://api.nuget.org/v3/index.json
 nuget restore -verbosity detailed -NonInteractive
 
 msbuild /p:Configuration=Release
