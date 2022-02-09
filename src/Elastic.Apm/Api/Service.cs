3edc805b6b9144357e8faf4e4abf443b9263b70b
Greg Kalapos <gergo@kalapos.net>
2022-02-09 17:45:34 +0100

    Update Service.cs

--- src/Elastic.Apm/Api/Service.cs
+++ src/Elastic.Apm/Api/Service.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to Elasticsearch B.V under one or more agreements.
+// Licensed to Elasticsearch B.V under one or more agreements.
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
@@ -85,7 +85,9 @@ namespace Elastic.Apm.Api
 		/// <param name="service"></param>
 		private static void CheckIfProfiler(Service service)
 		{
-			if (AppDomain.CurrentDomain.GetAssemblies().Where(n => n.FullName == "Elastic.Apm.Profiler.Managed").Any()) service.Agent.Version += "p";
+			if (AppDomain.CurrentDomain.GetAssemblies().Where(n => n.GetName().Name
+			.Equals("Elastic.Apm.Profiler.Managed", StringComparison.OrdinalIgnoreCase)).Any())
+				service.Agent.Version += "p";
 		}
 
 		public class AgentC
