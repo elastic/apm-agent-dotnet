9f232abd9152820062781e533fcf2b91a62b6bd6
Greg Kalapos <gergo@kalapos.net>
2022-03-16 19:45:07 +0100

    Update DbSpanName tests

--- test/Elastic.Apm.Tests/DbSpanNameTests.cs
+++ test/Elastic.Apm.Tests/DbSpanNameTests.cs
@@ -22,9 +22,9 @@ public class DbSpanNameTests
 	[Theory]
 	public void TestDbSignatures(string input, string output)
 	{
-		var signatureParser =  new SignatureParser(new Scanner());
+		var signatureParser = new SignatureParser(new Scanner());
 		var name = new StringBuilder();
-		signatureParser.QuerySignature(input.Replace(Environment.NewLine, " "), name,  preparedStatement: input.Length > 0);
+		signatureParser.QuerySignature(input.Replace(Environment.NewLine, " "), name, false);
 
 		name.ToString().ToLower().Should().Be(output.ToLower());
 	}
@@ -34,7 +34,7 @@ public class DbSpanNameTests
 		get
 		{
 			var _projectRoot = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-			var filePath= Path.GetFullPath(Path.Combine(_projectRoot, "TestResources" + Path.DirectorySeparatorChar + "db" + Path.DirectorySeparatorChar + "sql_signature_examples.json"));
+			var filePath = Path.GetFullPath(Path.Combine(_projectRoot, "TestResources" + Path.DirectorySeparatorChar + "db" + Path.DirectorySeparatorChar + "sql_signature_examples.json"));
 
 			using (var file = File.OpenText(filePath))
 			using (var reader = new JsonTextReader(file))
