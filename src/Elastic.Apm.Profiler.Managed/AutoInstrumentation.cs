9b130426cf9cbe382689c1fd4024a8e3a3c92d6e
Greg Kalapos <gergo@kalapos.net>
2022-04-12 18:58:03 +0200

    Enable AspNetCoreDiagnosticSubscriber in Profiler

--- src/Elastic.Apm.Profiler.Managed/AutoInstrumentation.cs
+++ src/Elastic.Apm.Profiler.Managed/AutoInstrumentation.cs
@@ -44,7 +44,12 @@ namespace Elastic.Apm.Profiler.Managed
 #endif
 				// ensure global instance is created if it's not already
 				if (!skipInstantiation)
+				{
 					_ = Agent.Instance;
+
+					Agent.Subscribe(new Elastic.Apm.AspNetCore.DiagnosticListener.AspNetCoreDiagnosticSubscriber());
+				//	Agent.Subscribe(new AspNetCoreDiagnosticSubscriber());
+				}
 			}
 			catch
 			{
