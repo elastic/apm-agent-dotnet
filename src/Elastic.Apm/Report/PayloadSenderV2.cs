881315275f18ca590412e5e9823c23215b43017a
Greg Kalapos <gergo@kalapos.net>
2022-08-16 23:57:37 +0200

    Update PayloadSenderV2.cs

--- src/Elastic.Apm/Report/PayloadSenderV2.cs
+++ src/Elastic.Apm/Report/PayloadSenderV2.cs
@@ -244,7 +244,8 @@ namespace Elastic.Apm.Report
 				_logger.Trace()?.Log("Waiting for data to send... (not using FlushInterval timer because FlushInterval is 0)");
 				try
 				{
-					receivedItems = _eventQueue.Receive(CancellationTokenSource.Token);
+					if(receivedItems == null)
+						receivedItems = _eventQueue.Receive(CancellationTokenSource.Token);
 				}
 				catch (OperationCanceledException)
 				{
