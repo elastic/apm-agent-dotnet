2f19646600fc5487dff61bef86c486b606540722
Wolfgang Ziegler <wziegler@live.at>
2022-09-15 16:46:36 +0200

    PR feedback

--- src/Elastic.Apm.AspNetCore/Extensions/RequestExtensions.cs
+++ src/Elastic.Apm.AspNetCore/Extensions/RequestExtensions.cs
@@ -2,8 +2,6 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
-using System;
-using System.Text;
 using Elastic.Apm.Config;
 using Elastic.Apm.Helpers;
 using Microsoft.AspNetCore.Http;
