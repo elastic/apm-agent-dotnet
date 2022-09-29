4ae48ecc51556753e89efb1ae6871fe7f929110b
Wolfgang Ziegler <wziegler@live.at>
2022-09-29 17:18:21 +0200

    Add and use JsonFileDataAttribute

---
+++ test/Elastic.Apm.Tests.Utilities/JsonFileDataAttribute.cs
@@ -0,0 +1,46 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Elastic.Apm.Libraries.Newtonsoft.Json.Linq;
+using Xunit.Sdk;
+
+namespace Elastic.Apm.Tests.Utilities;
+
+public class JsonFileDataAttribute : DataAttribute
+{
+	private readonly string _fileName;
+	private readonly Type _inputDataType;
+
+	public JsonFileDataAttribute(string fileName, Type inputDataType)
+	{
+		_fileName = fileName;
+		_inputDataType = inputDataType;
+	}
+
+	public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+	{
+		if (!File.Exists(_fileName))
+			throw new ArgumentException($"JSON input file {_fileName} does not exist");
+
+		var jToken = JToken.Parse(File.ReadAllText(_fileName));
+		switch (jToken.Type)
+		{
+			case JTokenType.Array:
+				foreach (var t in jToken)
+					yield return new []{ t.ToObject(_inputDataType) };
+				break;
+			case JTokenType.Object:
+				foreach (var kvp in (JObject)jToken)
+					yield return new [] { kvp.Key, kvp.Value.ToObject(_inputDataType) };
+				break;
+			default:
+				throw new Exception($"Unexpected JSON input: '{jToken}");
+		}
+	}
+}
