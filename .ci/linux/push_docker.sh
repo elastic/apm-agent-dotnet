76e1505be7f9cabab4e2a2a4be0bd28cb71b47ae
Greg Kalapos <gergo@kalapos.net>
2022-11-22 21:22:39 +0100

    Update push_docker.sh

--- .ci/linux/push_docker.sh
+++ .ci/linux/push_docker.sh
@@ -39,7 +39,7 @@ readonly CUR_TAG=${CUR_TAG:-$GIT_TAG_DEFAULT}
 
 # 2. Construct the image:tag that we are working with
 # This is roughly <repo>/<namespace>/image
-readonly DOCKER_PUSH_IMAGE="docker.elastic.co/observability/apm-agent-dotnet:$CUR_TAG"
+readonly DOCKER_PUSH_IMAGE="docker.elastic.co/observability/apm-agent-dotnet:1.18.0"
 
 # 3. Proceed with pushing to the registry
 readonly DOCKER_REGISTRY_URL=`echo $DOCKER_PUSH_IMAGE|cut -f1 -d/`
