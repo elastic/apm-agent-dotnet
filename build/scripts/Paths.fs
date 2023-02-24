22f7e69c144e7ae3ee835b1005e381349b6b6998
Martijn Laarman <Mpdreamz@gmail.com>
2023-02-24 10:32:55 +0100

    Fix snapshotty github action (#2016)

--- build/scripts/Paths.fs
+++ build/scripts/Paths.fs
@@ -16,11 +16,14 @@ module Paths =
     let SampleFolder = "sample"
     
     let BuildOutputFolder = sprintf "%s/output" BuildFolder
+    let PublishOutputFolder = sprintf "../../%s/output" BuildFolder
     
     /// A path to a folder in the build output folder
     let BuildOutput folder = sprintf "%s/%s" BuildOutputFolder folder
+    /// Used by --property:PublishDir relative to csproject
+    let PublishDir folder = sprintf "../../%s" <| BuildOutput folder
  
-    let NugetOutput = sprintf "%s/_packages" BuildOutputFolder
+    let NugetOutput = sprintf "%s/_packages" PublishOutputFolder
     
     /// All .NET Core and .NET Framework projects
     let Solution = "ElasticApmAgent.sln"
