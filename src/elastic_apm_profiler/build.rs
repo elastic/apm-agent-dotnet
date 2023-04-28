a014c85827a032b9d7c3771f5afcecf6fcbdb3af
Martijn Laarman <mpdreamz@gmail.com>
2023-04-28 17:25:22 +0200

    Update cargo_pkg_version and ensure custom AssemblyVersion anchor

--- src/elastic_apm_profiler/build.rs
+++ src/elastic_apm_profiler/build.rs
@@ -2,11 +2,21 @@ use std::process::Command;
 
 fn main() {
     static_vcruntime::metabuild();
-    
-    let output = Command::new("git")
+
+    let git_rev_parse = Command::new("git")
         .args(&["rev-parse", "HEAD"])
         .output()
         .unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
+
+    let git_hash = String::from_utf8(git_rev_parse.stdout).unwrap();
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+
+    let minver = Command::new("dotnet")
+        .args(&["minver", "-t=v", "-p=canary.0", "-v=e"])
+        .output()
+        .unwrap();
+
+    let version = String::from_utf8(minver.stdout).unwrap();
+    println!("cargo:rustc-env=CARGO_PKG_VERSION={}", version);
+    println!("cargo:warning=CARGO_PKG_VERSION={}", version);
 }
