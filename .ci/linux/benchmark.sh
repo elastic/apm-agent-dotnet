56d4c085395fd7443c633568e34b666624829fbe
Martijn Laarman <mpdreamz@gmail.com>
2023-05-23 20:49:34 +0200

    remove unreferenced .ci scripts

--- .ci/linux/benchmark.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# This script runs the benchamarks
-#
-set -euxo pipefail
-
-cd ./test/Elastic.Apm.Benchmarks
-dotnet run -c Release --filter AspNetCoreLoadTestWithAgent AspNetCoreLoadTestWithoutAgent
-dotnet run -c Release --filter *CollectAllMetrics2X* *Simple100Transaction10Spans* *SimpleTransactionsWith*
