19ac2da44e0a1c4c48fc9a34fed70dde70d84730
Martijn Laarman <Mpdreamz@gmail.com>
2023-05-28 11:40:09 +0200

    re-enable windows jobs

--- .ci/windows/tools.ps1
+++ .ci/windows/tools.ps1
@@ -7,13 +7,6 @@ Install-WindowsFeature -Name Web-Server, Web-Mgmt-Tools
 Add-WindowsFeature NET-Framework-45-ASPNET
 Add-WindowsFeature Web-Asp-Net45
 
-# Install .Net SDKs
-Write-Host "Install .Net SDKs"
-choco install dotnetcore-sdk -m -y --no-progress -r --version 3.1.416
-choco install dotnet-sdk -m -y --no-progress -r --version 5.0.100
-choco install dotnet-sdk -m -y --no-progress -r --version 6.0.101
-choco install dotnet-sdk -m -y --no-progress -r --version 7.0.100
-
 # Install NuGet Tool
 choco install nuget.commandline -y --no-progress -r --version 6.0.0
 
