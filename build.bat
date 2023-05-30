b21913eeeff89031e31626489bfe4be550d7ddf3
Martijn Laarman <Mpdreamz@gmail.com>
2023-05-30 11:23:55 +0200

    Remove PATH modification from build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,8 @@
 @echo Off
+<<<<<<< Updated upstream
 REM Use the installed cargo that happened when runnin .ci/windows/tools.ps1
 SET PATH=%PATH%;C:\tools\cargo
 dotnet run --project build\scripts -- %*
+=======
+dotnet run --project build -- %*
+>>>>>>> Stashed changes
