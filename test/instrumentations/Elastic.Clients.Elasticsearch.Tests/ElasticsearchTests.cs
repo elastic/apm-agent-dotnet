ab45515cf2a83fefcdd52e5283d0dd9d5e91f1b8
Martijn Laarman <Mpdreamz@gmail.com>
2023-10-16 11:51:13 +0200

    Improve error message when Elasticsearch integration test fails

--- test/instrumentations/Elastic.Clients.Elasticsearch.Tests/ElasticsearchTests.cs
+++ test/instrumentations/Elastic.Clients.Elasticsearch.Tests/ElasticsearchTests.cs
@@ -208,14 +208,14 @@ public class ElasticsearchTests : IClassFixture<ElasticsearchTestFixture>
 
 		var response = await _client.IndexAsync(tweet, request => request.Index("my-tweet-index"));
 
-		response.IsSuccess().Should().BeTrue();
+		response.IsSuccess().Should().BeTrue("{0}", response.DebugInformation);
 	}
 
 	private async Task GetDocumentAsync()
 	{
 		var response = await _client.GetAsync<Tweet>(1, idx => idx.Index("my-tweet-index"));
 
-		response.IsSuccess().Should().BeTrue();
+		response.IsSuccess().Should().BeTrue("{0}", response.DebugInformation);
 		var tweet = response.Source;
 		tweet.Should().NotBeNull();
 	}
@@ -241,14 +241,14 @@ public class ElasticsearchTests : IClassFixture<ElasticsearchTestFixture>
 	private async Task UpdateDocumentAsync(Tweet tweet)
 	{
 		tweet.Message = "This is a new message";
-		var response2 = await _client.UpdateAsync<Tweet, object>("my-tweet-index", 1, u => u
+		var response = await _client.UpdateAsync<Tweet, object>("my-tweet-index", 1, u => u
 			.Doc(tweet));
-		response2.IsValidResponse.Should().BeTrue();
+		response.IsValidResponse.Should().BeTrue("{0}", response.DebugInformation);
 	}
 
 	private async Task DeleteDocumentAsync()
 	{
 		var response = await _client.DeleteAsync("my-tweet-index", 1);
-		response.IsValidResponse.Should().BeTrue();
+		response.IsValidResponse.Should().BeTrue("{0}", response.DebugInformation);
 	}
 }
