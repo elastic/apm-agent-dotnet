f3ac10c8203658b0ab7dd150ccf2b0aeca0757e9
Martijn Laarman <Mpdreamz@gmail.com>
2023-10-16 14:57:36 +0200

    remove unused message sink logger

--- test/instrumentations/Elastic.Clients.Elasticsearch.Tests/ElasticsearchTestFixture.cs
+++ test/instrumentations/Elastic.Clients.Elasticsearch.Tests/ElasticsearchTestFixture.cs
@@ -3,17 +3,13 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
-using DotNet.Testcontainers.Builders;
-using DotNet.Testcontainers.Configurations;
 using DotNet.Testcontainers.Containers;
 using Elastic.Transport;
-using Microsoft.Extensions.Logging;
 using Testcontainers.Elasticsearch;
 using Xunit;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 using HttpMethod = Elastic.Transport.HttpMethod;
-using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Elastic.Clients.Elasticsearch.Tests;
 
@@ -74,23 +70,5 @@ public sealed class ElasticsearchTestFixture : IAsyncLifetime
 			await Container.StopAsync();
 			await Container.DisposeAsync();
 		}
-	}
-
-	private class MessageSinkLogger : ILogger
-	{
-		private readonly IMessageSink _messageSink;
-
-		public IDisposable? BeginScope<TState>(TState state) where TState : notnull => null;
-
-		public MessageSinkLogger(IMessageSink sink)
-		{
-			_messageSink = sink;
-			_messageSink.OnMessage(new DiagnosticMessage($"Started {nameof(MessageSinkLogger)}"));
-		}
-
-		public bool IsEnabled(LogLevel logLevel) => true;
-
-		public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter) =>
-			_messageSink.OnMessage(new DiagnosticMessage(formatter(state, exception)));
 	}
 }
