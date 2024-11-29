1b83a20f5430cd30d3d1566ad7d6cbb981d4d240
Steve Gordon <sgordon@hotmail.co.uk>
2024-11-29 12:27:16 +0000

    Add timeout to Docker check

--- test/Elastic.Apm.Tests.Utilities/XUnit/DockerAttributes.cs
+++ test/Elastic.Apm.Tests.Utilities/XUnit/DockerAttributes.cs
@@ -67,7 +67,7 @@ internal static class DockerUtils
 	{
 		try
 		{
-			var result = Proc.Start(new StartArguments("docker", "--version"));
+			var result = Proc.Start(new StartArguments("docker", "--version"), TimeSpan.FromSeconds(30));
 			HasDockerInstalled = result.ExitCode == 0;
 		}
 		catch (Exception)
