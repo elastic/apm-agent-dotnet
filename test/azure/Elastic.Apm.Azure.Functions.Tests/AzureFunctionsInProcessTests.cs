1a0945bff230c1eb7e139cf406ff265d7961af94
Jeremy Besson <jeremy.besson@elastic.co>
2025-10-06 10:37:58 +0300

    Enable again disabled  CI AzureFunctionsInProcessTests

--- test/azure/Elastic.Apm.Azure.Functions.Tests/AzureFunctionsInProcessTests.cs
+++ test/azure/Elastic.Apm.Azure.Functions.Tests/AzureFunctionsInProcessTests.cs
@@ -18,7 +18,7 @@ public class AzureFunctionsInProcessTests : AzureFunctionsTestBase, IClassFixtur
 	public AzureFunctionsInProcessTests(ITestOutputHelper output, InProcessContext context)
 		: base(output, context) { }
 
-	[FlakyCiTestFact(2501)]
+	[Fact]
 	public async Task Invoke_Http_Ok()
 	{
 		var transaction = await InvokeAndAssertFunction(SampleHttpTrigger);
@@ -28,7 +28,7 @@ public class AzureFunctionsInProcessTests : AzureFunctionsTestBase, IClassFixtur
 		transaction.Context.Response.StatusCode.Should().Be(200);
 	}
 
-	[FlakyCiTestFact(2501)]
+	[Fact]
 	public async Task Invoke_Http_InternalServerError()
 	{
 		var transaction = await InvokeAndAssertFunction(HttpTriggerWithInternalServerError);
@@ -38,7 +38,7 @@ public class AzureFunctionsInProcessTests : AzureFunctionsTestBase, IClassFixtur
 		transaction.Context.Response.StatusCode.Should().Be(500);
 	}
 
-	[FlakyCiTestFact(2501)]
+	[Fact]
 	public async Task Invoke_Http_FunctionThrowsException()
 	{
 		var transaction = await InvokeAndAssertFunction(HttpTriggerWithException);
@@ -49,7 +49,7 @@ public class AzureFunctionsInProcessTests : AzureFunctionsTestBase, IClassFixtur
 		transaction.Context.Response.StatusCode.Should().Be(500);
 	}
 
-	[FlakyCiTestFact(2501)]
+	[Fact]
 	public async Task Invoke_Http_NotFound()
 	{
 		var transaction = await InvokeAndAssertFunction(HttpTriggerWithNotFound);
