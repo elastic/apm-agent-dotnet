0ac38227e9f6fa93e478a4e9fe1e06be73258261
Jeremy Besson <jeremy.besson@elastic.co>
2025-10-07 15:23:08 +0300

    Test

--- test/azure/Elastic.Apm.Azure.Functions.Tests/AzureFunctionsTestFixture.cs
+++ test/azure/Elastic.Apm.Azure.Functions.Tests/AzureFunctionsTestFixture.cs
@@ -137,7 +137,7 @@ public abstract class AzureFunctionTestContextBase : IDisposable
 		var transactionName = $"GET {uri.AbsolutePath}";
 		output.WriteLine($"Invoking {uri} ...");
 		var attempt = 0;
-		const int maxAttempts = 100;
+		const int maxAttempts = 60;
 		using var httpClient = new HttpClient();
 		httpClient.DefaultRequestHeaders.Add("traceparent", "00-0af7651916cd43dd8448eb211c80319c-b7ad6b7169203331-01");
 		while (++attempt < maxAttempts)
